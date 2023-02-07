5679af336c09bd552c1e85b7ed616c49e0fa77f6
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-07 14:09:51 +0100

    feat(page): clean home page & add custom stylesheet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,10 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import './page.scss'
 
-const inter = Inter({ subsets: ['latin'] })
-
-// eslint-disable-next-line max-lines-per-function
 export default function Home(): JSX.Element {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            By{' '}
-            <Image
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              height={24}
-              priority
-              src="/vercel.svg"
-              width={100}
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          alt="Next.js Logo"
-          className={styles.logo}
-          height={37}
-          priority
-          src="/next.svg"
-          width={180}
-        />
-        <div className={styles.thirteen}>
-          <Image alt="13" height={31} priority src="/thirteen.svg" width={40} />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          className={styles.card}
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          className={styles.card}
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          className={styles.card}
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="main">
+      <div>
+        <p className="text">Dataverse Portal</p>
       </div>
     </main>
   )
