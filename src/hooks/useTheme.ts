42e19753621de9b1575a5ad73b04056fa7c23e80
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-07 14:08:10 +0100

    feat(hook): implement useTheme

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,11 @@
+import { useContext } from 'react'
+import type { ThemeContextType } from '@/context/themeContext'
+import ThemeContext from '@/context/themeContext'
+
+export const useTheme = (): ThemeContextType => {
+  const themeContext = useContext(ThemeContext)
+  if (themeContext === null) {
+    throw new Error('Theme context not found...')
+  }
+  return themeContext
+}
