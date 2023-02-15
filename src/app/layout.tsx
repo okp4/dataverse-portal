742f552c6a18d0d4e1659fbb1d565e4aa3862660
lauren olivier <laurenzolivier@gmail.com>
2023-02-15 15:35:01 +0100

    feat: remove head from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,6 @@ import { ThemeProvider } from '@/components/providers/themeProvider'
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
