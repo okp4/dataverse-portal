c594ea2d092f5bdd856b5f8d5b84c62bfad43ed4
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-15 21:23:42 +0100

    feat(ui): enhance themeProvider with localStorage

--- src/components/providers/themeProvider.tsx
+++ src/components/providers/themeProvider.tsx
@@ -1,19 +1,30 @@
 'use client'
-
 import type { FC } from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import type { Theme } from '@/context/themeContext'
 import ThemeContext from '@/context/themeContext'
+import type { LocalStorageState } from '@/hooks/useLocalStorage'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
 
 type ThemeProviderProps = Readonly<{
   children: React.ReactNode
 }>
 
-export const ThemeProvider: FC<ThemeProviderProps> = ({ children }: ThemeProviderProps) => {
+const ThemeProvider: FC<ThemeProviderProps> = ({ children }: ThemeProviderProps) => {
   const [theme, setTheme] = useState<Theme>('light')
+  const [value]: LocalStorageState = useLocalStorage('okp4-theme')
+
+  useEffect(() => {
+    if (!value) {
+      return
+    }
+    setTheme(value as Theme)
+  }, [setTheme, value])
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className={`theme--${theme}`}>{children}</div>
     </ThemeContext.Provider>
   )
 }
+
+export default ThemeProvider
