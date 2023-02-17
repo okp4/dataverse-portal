c508dc16f9c5051bdfe373109d82a192a1467ca4
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-17 16:57:47 +0100

    feat(ui): update app component with theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import type { FC } from 'react'
-import React, { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.scss'
+import React, { useCallback } from 'react'
+import { useAppStore } from './store/appStore'
 
 const App: FC = () => {
-  const [count, setCount] = useState(0)
-
-  const handleClick = useCallback(() => {
-    setCount(count => count + 1)
-  }, [])
-
+  const theme = useAppStore(store => store.theme)
+  const setTheme = useAppStore(store => store.setTheme)
+  const handleTheme = useCallback(() => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }, [setTheme, theme])
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img alt="Vite logo" className="logo" src="/vite.svg" />
-        </a>
-        <a href="https://reactjs.org" rel="noreferrer" target="_blank">
-          <img alt="React logo" className="logo react" src={reactLogo} />
-        </a>
+    <div className={`theme--${theme}`}>
+      <div className="okp4-dataverse-portal-main">
+        <button onClick={handleTheme}>ClickMe</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   )
 }
