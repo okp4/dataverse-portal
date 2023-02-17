babd50aa0ef4e985acb7941bd38f6a18659f591e
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-17 09:51:07 +0100

    fix: make linters happy

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
+import type { FC } from 'react'
+import React, { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
 
-function App() {
+const App: FC = () => {
   const [count, setCount] = useState(0)
+
+  const handleClick = useCallback(() => {
+    setCount(count => count + 1)
+  }, [])
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+          <img alt="Vite logo" className="logo" src="/vite.svg" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" rel="noreferrer" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClick}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   )
 }
