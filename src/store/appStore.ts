6bd7f27c01a65e98b1a464194950f951bef35826
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-19 10:28:18 +0100

    feat(store): update store with sidebar slice

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -3,16 +3,19 @@ import { immer } from 'zustand/middleware/immer'
 import { persist, createJSONStorage } from 'zustand/middleware'
 import type { ThemeSlice } from './slice/theme.slice'
 import { createThemeSlice } from './slice/theme.slice'
+import type { SidebarSlice } from './slice/sidebar.slice'
+import { createSidebarSlice } from './slice/sidebar.slice'
 
-export const useAppStore = create<ThemeSlice>()(
+export const useAppStore = create<ThemeSlice & SidebarSlice>()(
   persist(
     immer((...a) => ({
-      ...createThemeSlice(...a)
+      ...createThemeSlice(...a),
+      ...createSidebarSlice(...a)
     })),
     {
       name: 'okp4-storage',
       storage: createJSONStorage(() => localStorage),
-      partialize: state => ({ theme: state.theme })
+      partialize: state => ({ theme: state.theme, isSidebarExpanded: state.isSidebarExpanded })
     }
   )
 )
