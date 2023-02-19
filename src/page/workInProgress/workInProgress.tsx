aa555ad057d2f5162e4b990ea5d0890c4682864c
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-19 10:31:54 +0100

    feat(ui): add basic WIP page

---
+++ src/page/workInProgress/workInProgress.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react'
+import './workInProgress.scss'
+
+export const WorkInProgress: FC = () => (
+  <div className="okp4-dataverse-portal-work-in-progress-main">
+    <h1>Work in progress</h1>
+    <p>We are working hard to unleash the Dataverse</p>
+  </div>
+)
