7fae9f3b262ddb87d07567c726ee8d8b3b4cb23b
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-20 21:42:14 +0100

    feat(i18n): declare i18n instance

---
+++ src/i18n/index.ts
@@ -0,0 +1,40 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      lookupLocalStorage: 'i18nextLng',
+      caches: ['localStorage'],
+      htmlTag: document.documentElement
+    },
+    resources: {},
+    fallbackLng: 'en',
+    fallbackNS: 'translation',
+    interpolation: {
+      escapeValue: true,
+      format: (value, format): string => {
+        if (!value) {
+          return ''
+        }
+        if (format === 'uppercase') {
+          return value.toUpperCase()
+        }
+        if (format === 'lowercase') {
+          return value.toLowerCase()
+        }
+        return value
+      }
+    },
+    react: {
+      useSuspense: true
+    },
+    returnNull: false
+  })
+
+export default i18n
