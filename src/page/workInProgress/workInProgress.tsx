b325c7876fb16fef5ce7bc02d044f3b55b6c9d23
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-20 21:46:32 +0100

    feat(i18n): use translations in workInProgress page

--- src/page/workInProgress/workInProgress.tsx
+++ src/page/workInProgress/workInProgress.tsx
@@ -1,9 +1,14 @@
 import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import './workInProgress.scss'
+import './i18n/index'
 
-export const WorkInProgress: FC = () => (
-  <div className="okp4-dataverse-portal-work-in-progress-main">
-    <h1>Work in progress</h1>
-    <p>We are working hard to unleash the Dataverse</p>
-  </div>
-)
+export const WorkInProgress: FC = () => {
+  const { t } = useTranslation('workInProgress')
+  return (
+    <div className="okp4-dataverse-portal-work-in-progress-main">
+      <h1>{t('workInProgress.title')}</h1>
+      <p>{t('workInProgress.description')}</p>
+    </div>
+  )
+}
