5fc3b8f0727fc5e20342311614f844983c52cecd
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-20 21:43:10 +0100

    feat(i18n): add i18n utilitary methods

---
+++ src/i18n/utils.ts
@@ -0,0 +1,29 @@
+import type { JSONValue } from '@/superTypes'
+import i18n from './index'
+
+export type I18nResource = Readonly<{
+  readonly lng: string
+  readonly namespace: string
+  readonly resource: JSONValue
+}>
+
+export const loadTranslations = (resources: Array<I18nResource>, overwrite?: boolean): void => {
+  resources.forEach((resource: I18nResource) =>
+    i18n.addResourceBundle(
+      resource.lng,
+      resource.namespace,
+      resource.resource,
+      !overwrite,
+      overwrite
+    )
+  )
+}
+
+export const updateLanguage = (lng: string): void => {
+  i18n.changeLanguage(lng)
+}
+
+export const isCurrentLanguage = (lng: string): boolean => {
+  const languageValueRegex = new RegExp(lng, 'i')
+  return languageValueRegex.test(i18n.language)
+}
