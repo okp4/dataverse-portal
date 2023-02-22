c510375472fb688f4eb0a1bcf0807956a3c22e24
ccamel <camel.christophe@gmail.com>
2023-02-22 16:18:18 +0100

    feat: configure html favicon and metas

---
+++ config/html-config.ts
@@ -0,0 +1,60 @@
+import { URL } from 'url'
+import type { Options } from 'vite-plugin-html-config'
+import pkg from '../package.json'
+
+const options: Options = {
+  favicon: '/favicon.svg',
+  title: 'OKP4 Dataverse Portal',
+  metas: [
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0'
+    },
+    {
+      name: 'description',
+      content: pkg.description
+    },
+    {
+      name: 'author',
+      content: pkg.author.name
+    },
+    {
+      name: 'og:type',
+      content: 'website'
+    },
+    {
+      name: 'og:title',
+      content: 'OKP4 Dataverse Portal'
+    },
+    {
+      name: 'og:description',
+      content: pkg.description
+    },
+    {
+      name: 'keywords',
+      content: [
+        'OKP4',
+        'Portal',
+        'Data App',
+        'Dataverse',
+        'Services',
+        'Dataset',
+        'Data Space',
+        'Deposit',
+        'Catalog',
+        'Files',
+        'Blockchain',
+        'Metadata',
+        'Exploration',
+        'Know',
+        'Token'
+      ].join(', ')
+    },
+    {
+      name: 'version',
+      content: pkg.version
+    }
+  ]
+}
+
+export default options
