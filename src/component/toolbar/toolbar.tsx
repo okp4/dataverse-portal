a764090d6f07cc0ddcc077f87d8f547bba77f91d
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-02-22 16:53:55 +0100

    feat(ui): use toolbar translations

--- src/component/toolbar/toolbar.tsx
+++ src/component/toolbar/toolbar.tsx
@@ -1,14 +1,17 @@
 import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Button } from '../button/button'
 import { Icon } from '../icon/icon'
 import './toolbar.scss'
+import './i18n/index'
 
 export const Toolbar: FC = () => {
+  const { t } = useTranslation('toolbar')
   return (
     <div className="okp4-dataverse-portal-toolbar-main">
       <Button
         icons={{ startIcon: <Icon name="wallet" /> }}
-        label="Connect my wallet"
+        label={t('toolbar.connectWallet')}
         variant="tertiary"
       />
     </div>
