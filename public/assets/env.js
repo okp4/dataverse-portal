83b1d0121e399b876de6ca2e049079f0c1659c83
ccamel <camel.christophe@gmail.com>
2023-03-03 08:48:40 +0100

    build(project): add default environment file

---
+++ public/assets/env.js
@@ -0,0 +1,10 @@
+const settings = {
+    urls: {
+        'social:twitter': 'https://twitter.com/OKP4_Protocol',
+        'social:linkedin': 'https://www.linkedin.com/company/okp4-open-knowledge-platform-for/',
+        'social:discord': 'https://discord.com/invite/okp4',
+        'social:medium': 'https://blog.okp4.network',
+        'form:feedback': 'https://okp4.typeform.com/to/TNyFBH72',
+        'about:okp4': 'https://okp4.network',
+      }
+}
