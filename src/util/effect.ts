70ece341b0f447cf42b328c8e33fe45c7217b5fa
ccamel <camel.christophe@gmail.com>
2023-03-06 12:11:18 +0100

    style: specialize type to remove 'any'

--- src/util/effect.ts
+++ src/util/effect.ts
@@ -4,18 +4,15 @@ import type { IO } from 'fp-ts/IO'
 // Defines the type of function that returns an IO or a Task also known as a computational function.
 export type ComputationalFunc = (...args: unknown[]) => IO<unknown> | Task<unknown>
 
+type EffectfulResultType<F extends ComputationalFunc> = ReturnType<F> extends IO<infer T>
+  ? T
+  : ReturnType<F> extends Task<infer U>
+  ? Promise<U>
+  : never
+
 // toEffectful is an utility function that converts a computational function (i.e a function that returns an IO or a Task) to a function that returns
 // its result directly (producing a side effect).
 export const toEffectful =
-  <F extends ComputationalFunc> (
-    fn: F
-  ): ((
-    ...args: Parameters<F>
-  ) => ReturnType<F> extends IO<infer T>
-    ? T
-    : ReturnType<F> extends Task<infer U>
-    ? Promise<U>
-    : never) =>
+  <F extends ComputationalFunc>(fn: F): ((...args: Parameters<F>) => EffectfulResultType<F>) =>
   (...args: Parameters<F>) =>
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    fn(...args)() as any
+    fn(...args)() as EffectfulResultType<F>
