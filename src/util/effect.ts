f4dcc535704f3eace3176d36dca240d2b525211c
ccamel <camel.christophe@gmail.com>
2023-03-07 10:22:00 +0100

    style(util): improve functional style

--- src/util/effect.ts
+++ src/util/effect.ts
@@ -11,21 +11,14 @@ export type ToEffectful<T> = T extends (...args: infer Args) => IO<infer R>
 
 // toEffectful is an utility function that converts a computational function (i.e a function that returns an IO or a Task) to a function that returns
 // its result directly (producing a side effect), else it does nothing.
-export const toEffectful = <T>(v: T): ToEffectful<T> => {
-  if (typeof v !== 'function') {
-    return v as ToEffectful<T>
-  }
+export const toEffectful = <T>(v: T): ToEffectful<T> =>
+  typeof v !== 'function'
+    ? (v as ToEffectful<T>)
+    : (((...args: unknown[]) => {
+        const result = v(...args)
 
-  return ((...args: unknown[]) => {
-    const result = v(...args)
-
-    if (typeof result === 'function') {
-      return result()
-    }
-
-    return result
-  }) as ToEffectful<T>
-}
+        return typeof result === 'function' ? result() : result
+      }) as ToEffectful<T>)
 
 // ToEffectfulObject is a utility type that converts a computational object (i.e an object that contains computational functions) to an object that
 // contains functions that return their result directly (producing a side effect), leaving the type of the other properties unmodified.
@@ -40,9 +33,11 @@ export type ToEffectfulObject<T extends Record<string, unknown>> = {
 // toEffectfulObject is an utility function that converts a computational object (i.e an object that contains computational functions) to an object that
 // contains functions that return their result directly (producing a side effect), leaving the rest of the object untouched.
 export function toEffectfulObject<T extends Record<string, unknown>>(obj: T): ToEffectfulObject<T> {
-  return Object.keys(obj).reduce((result: Record<string, unknown>, key: string) => {
-    result[key] = toEffectful(obj[key])
-
-    return result
-  }, {}) as ToEffectfulObject<T>
+  return Object.keys(obj).reduce(
+    (result: Record<string, unknown>, key: string) => ({
+      ...result,
+      [key]: toEffectful(obj[key])
+    }),
+    {}
+  ) as ToEffectfulObject<T>
 }
