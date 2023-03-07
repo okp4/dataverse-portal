266bfb7b094d3a52d310349d611c871d9b245576
ccamel <camel.christophe@gmail.com>
2023-03-07 09:03:34 +0100

    refactor(util): generatlise toEffecful function

--- src/util/effect.ts
+++ src/util/effect.ts
@@ -1,18 +1,28 @@
 import type { Task } from 'fp-ts/Task'
 import type { IO } from 'fp-ts/IO'
 
-// Defines the type of function that returns an IO or a Task also known as a computational function.
-export type ComputationalFunc = (...args: unknown[]) => IO<unknown> | Task<unknown>
-
-type EffectfulResultType<F extends ComputationalFunc> = ReturnType<F> extends IO<infer T>
-  ? T
-  : ReturnType<F> extends Task<infer U>
-  ? Promise<U>
-  : never
+// ToEffectful is a utility type that converts a computational function (i.e a function that returns an IO or a Task) to a function that returns
+// its result directly (producing a side effect), else it returns the same type.
+export type ToEffectful<T> = T extends (...args: infer Args) => IO<infer R>
+  ? (...args: Args) => R
+  : T extends (...args: infer Args) => Task<infer R>
+  ? (...args: Args) => Promise<R>
+  : T
 
 // toEffectful is an utility function that converts a computational function (i.e a function that returns an IO or a Task) to a function that returns
-// its result directly (producing a side effect).
-export const toEffectful =
-  <F extends ComputationalFunc>(fn: F): ((...args: Parameters<F>) => EffectfulResultType<F>) =>
-  (...args: Parameters<F>) =>
-    fn(...args)() as EffectfulResultType<F>
+// its result directly (producing a side effect), else it does nothing.
+export const toEffectful = <T>(v: T): ToEffectful<T> => {
+  if (typeof v !== 'function') {
+    return v as ToEffectful<T>
+  }
+
+  return ((...args: unknown[]) => {
+    const result = v(...args)
+
+    if (typeof result === 'function') {
+      return result()
+    }
+
+    return result
+  }) as ToEffectful<T>
+}
