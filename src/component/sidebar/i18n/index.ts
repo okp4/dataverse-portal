7d1b801fbfed6912fd1aaed6e2fe56578574e2c1
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-03-08 18:32:47 +0100

    feat(i18n): add fr & de translations for Sidebar component

--- src/component/sidebar/i18n/index.ts
+++ src/component/sidebar/i18n/index.ts
@@ -1,7 +1,13 @@
 import sidebar_en from './sidebar_en.json'
+import sidebar_fr from './sidebar_fr.json'
+import sidebar_de from './sidebar_de.json'
 import type { I18nResource } from '@/i18n/utils'
 import { loadTranslations } from '@/i18n/utils'
 
-const i18nTranslations: I18nResource[] = [{ lng: 'en', namespace: 'sidebar', resource: sidebar_en }]
+const i18nTranslations: I18nResource[] = [
+  { lng: 'en', namespace: 'sidebar', resource: sidebar_en },
+  { lng: 'fr', namespace: 'sidebar', resource: sidebar_fr },
+  { lng: 'de', namespace: 'sidebar', resource: sidebar_de }
+]
 
 loadTranslations(i18nTranslations)
