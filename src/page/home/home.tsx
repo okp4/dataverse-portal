7e3f6d85525c4b5f572864570c4a723548b006d9
ErikssonJoakim <j.eriksson@okp4.com>
2023-03-09 19:45:35 +0100

    refactor(home): iterate stats

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { shallow } from 'zustand/shallow'
 import { Card } from '@/component/card/card'
@@ -8,54 +9,72 @@ import { useAppStore } from '@/store/appStore'
 import './home.scss'
 import './i18n/index'
 
+type Stats = {
+  icon?: JSX.Element
+  stat: number
+  description: string
+}
+
 const Stats: FC = () => {
   const { t } = useTranslation('home')
   const theme = useAppStore(store => store.theme)
-  const { dataspacesCreatedNb, datasetsSharedNb, servicesSharedNb } = useStatsAggregate(
-    aggregate => ({
-      dataspacesCreatedNb: aggregate.dataspacesCreatedNb,
-      datasetsSharedNb: aggregate.datasetsSharedNb,
-      servicesSharedNb: aggregate.servicesSharedNb
-    }),
-    shallow
+  const { dataspacesCreatedNb, datasetsSharedNb, servicesSharedNb } =
+    useStatsAggregate(
+      aggregate => ({
+        dataspacesCreatedNb: aggregate.dataspacesCreatedNb,
+        datasetsSharedNb: aggregate.datasetsSharedNb,
+        servicesSharedNb: aggregate.servicesSharedNb
+      }),
+      shallow
+    )
+
+  const stats: Stats[] = useMemo(
+    () => [
+      
+      {
+        icon: <Icon name={`dataspace-created-${theme}`} />,
+        stat: dataspacesCreatedNb,
+        description: t('home.blocks.community.stats.dataspacesCreated')
+      },
+      {
+        icon: <Icon name={`dataset-folder-${theme}`} />,
+        stat: datasetsSharedNb,
+        description: t('home.blocks.community.stats.datasetsShared')
+      },
+      {
+        icon: <Icon name={`service-folder-${theme}`} />,
+        stat: servicesSharedNb,
+        description: t('home.blocks.community.stats.servicesShared')
+      }
+    ],
+    [ datasetsSharedNb, dataspacesCreatedNb, servicesSharedNb, t, theme]
   )
+
   return (
-    <Card>
-      <div className="okp4-dataverse-portal-home-page-stats-container">
-        <div className="okp4-dataverse-portal-home-page-stats-content-wrapper">
-          <div className="okp4-dataverse-portal-home-page-stats-content-icon-container">
-            <Icon name={`dataspace-created-${theme}`} />
-          </div>
-          <div>
-            <h2>{dataspacesCreatedNb}</h2>
-            <p>{t('home.blocks.community.stats.dataspacesCreated')}</p>
-          </div>
+   
+      <Card>
+        <div className="okp4-dataverse-portal-home-page-stats-container">
+          {stats.slice(1, 4).map(({ icon, stat, description }, index) => (
+            <div className="okp4-dataverse-portal-home-page-stats-content-wrapper" key={index}>
+              {icon && (
+                <div className="okp4-dataverse-portal-home-page-stats-content-icon-container">
+                  {icon}
+                </div>
+              )}
+              <div>
+                <h2>{stat}</h2>
+                <p>{description}</p>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="okp4-dataverse-portal-home-page-stats-content-wrapper">
-          <div className="okp4-dataverse-portal-home-page-stats-content-icon-container">
-            <Icon name={`dataset-folder-${theme}`} />
-          </div>
-          <div>
-            <h2>{datasetsSharedNb}</h2>
-            <p>{t('home.blocks.community.stats.datasetsShared')}</p>
-          </div>
-        </div>
-        <div className="okp4-dataverse-portal-home-page-stats-content-wrapper">
-          <div className="okp4-dataverse-portal-home-page-stats-content-icon-container">
-            <Icon name={`service-folder-${theme}`} />
-          </div>
-          <div>
-            <h2>{servicesSharedNb}</h2>
-            <p>{t('home.blocks.community.stats.servicesShared')}</p>
-          </div>
-        </div>
-      </div>
-    </Card>
+      </Card>
   )
 }
 
 export const Home: FC = () => {
   const { t } = useTranslation('home')
+
   return (
     <div className="okp4-dataverse-portal-home-page-main">
       <div className="okp4-dataverse-portal-home-page-block-container-community">
