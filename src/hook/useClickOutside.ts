16018ccc5d34ca7c8931fcd886281952e25b9a97
Frédéric Vilcot <47554752+fredericvilcot@users.noreply.github.com>
2023-03-09 11:27:41 +0100

    refactor(hook): improve useOnClickOutside function
    Co-authored-by: Chris <camel.christophe@gmail.com>

--- src/hook/useClickOutside.ts
+++ src/hook/useClickOutside.ts
@@ -1,14 +1,18 @@
 import type { RefObject } from 'react'
 import { useCallback, useEffect } from 'react'
 
-type EventParameter = MouseEvent | TouchEvent
+type Event = MouseEvent | TouchEvent
 
-export const useClickOutside = <T extends HTMLElement = HTMLElement>(
+// Callback is the type of the function called when the user clicks outside the element.
+export type Callback = (event: Event) => void
+
+// useOnClickOutside is a custom hook that calls the callback function when the user clicks outside the given element.
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
   ref: RefObject<T>,
-  callback: (event: EventParameter) => void
+  callback: Callback
 ): void => {
   const listener = useCallback(
-    (event: EventParameter) => {
+    (event: Event) => {
       if (!ref.current || ref.current.contains(event.target as Node)) return
       callback(event)
     },
