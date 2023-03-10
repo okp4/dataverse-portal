ae595ec0a314c27d640524c657644d5b8b8dad83
lauren olivier <laurenzolivier@gmail.com>
2023-03-10 17:33:11 +0100

    feat(toolbar): add mobile and tablet toolbar versions

--- src/component/toolbar/toolbar.tsx
+++ src/component/toolbar/toolbar.tsx
@@ -1,19 +1,40 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useBreakpoint } from '@/hook/useBreakpoint'
+import { Okp4Logo } from '../logo/okp4Logo'
 import { Button } from '../button/button'
 import { Icon } from '../icon/icon'
 import './toolbar.scss'
 import './i18n/index'
 
+const renderSmallToolbar = (isMobile: boolean, walletLargeButton: JSX.Element): JSX.Element => (
+  <>
+    <div className="okp4-dataverse-portal-toolbar-logo">
+      <Okp4Logo />
+    </div>
+    {isMobile ? (
+      <Button iconButtonOnly={<Icon name="wallet" />} variant="tertiary" />
+    ) : (
+      walletLargeButton
+    )}
+  </>
+)
+
 export const Toolbar: FC = () => {
   const { t } = useTranslation('toolbar')
+  const { isMobile, isTablet } = useBreakpoint()
+
+  const walletLargeButton = (
+    <Button
+      icons={{ startIcon: <Icon name="wallet" /> }}
+      label={t('toolbar.connectWallet')}
+      variant="tertiary"
+    />
+  )
+
   return (
     <div className="okp4-dataverse-portal-toolbar-main">
-      <Button
-        icons={{ startIcon: <Icon name="wallet" /> }}
-        label={t('toolbar.connectWallet')}
-        variant="tertiary"
-      />
+      {isMobile || isTablet ? renderSmallToolbar(isMobile, walletLargeButton) : walletLargeButton}
     </div>
   )
 }
