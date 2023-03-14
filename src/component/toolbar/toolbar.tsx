22ae1b2cbbdf77ec960f5bea7526ed54e6ad7716
lauren olivier <laurenzolivier@gmail.com>
2023-03-14 11:58:34 +0100

    feat(toolbar): add responsivity

--- src/component/toolbar/toolbar.tsx
+++ src/component/toolbar/toolbar.tsx
@@ -1,40 +1,65 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { shallow } from 'zustand/shallow'
 import { useBreakpoint } from '@/hook/useBreakpoint'
+import { useAppStore } from '@/store/appStore'
+import type { Theme } from '@/store/slice/theme.slice'
 import { Okp4Logo } from '../logo/okp4Logo'
 import { Button } from '../button/button'
 import { Icon } from '../icon/icon'
 import './toolbar.scss'
 import './i18n/index'
 
-const renderSmallToolbar = (isMobile: boolean, walletLargeButton: JSX.Element): JSX.Element => (
-  <>
-    <div className="okp4-dataverse-portal-toolbar-logo">
-      <Okp4Logo />
-    </div>
-    {isMobile ? (
+type BurgerProps = {
+  expandSidebar: () => void
+  theme: Theme
+}
+
+type WalletButtonProps = {
+  isSmallScreen: boolean
+  label: string
+}
+
+const BurgerMenu = ({ expandSidebar, theme }: BurgerProps): JSX.Element => (
+  <div className="okp4-dataverse-portal-burger-menu" onClick={expandSidebar}>
+    <Button iconButtonOnly={<Icon name={`burger-menu-${theme}`} />} variant="tertiary" />
+  </div>
+)
+
+const WalletButton = ({ isSmallScreen, label }: WalletButtonProps): JSX.Element => (
+  <div className="okp4-dataverse-portal-wallet">
+    {isSmallScreen ? (
       <Button iconButtonOnly={<Icon name="wallet" />} variant="tertiary" />
     ) : (
-      walletLargeButton
+      <Button icons={{ startIcon: <Icon name="wallet" /> }} label={label} variant="tertiary" />
     )}
-  </>
+  </div>
 )
 
 export const Toolbar: FC = () => {
   const { t } = useTranslation('toolbar')
   const { isMobile, isTablet } = useBreakpoint()
-
-  const walletLargeButton = (
-    <Button
-      icons={{ startIcon: <Icon name="wallet" /> }}
-      label={t('toolbar.connectWallet')}
-      variant="tertiary"
-    />
+  const isSmallScreen = isMobile || isTablet
+  const { expandSidebar, isSidebarExpanded, theme } = useAppStore(
+    state => ({
+      expandSidebar: state.expandSidebar,
+      isSidebarExpanded: state.isSidebarExpanded,
+      theme: state.theme
+    }),
+    shallow
   )
 
   return (
     <div className="okp4-dataverse-portal-toolbar-main">
-      {isMobile || isTablet ? renderSmallToolbar(isMobile, walletLargeButton) : walletLargeButton}
+      {isSmallScreen && (
+        <>
+          {!isSidebarExpanded && <BurgerMenu expandSidebar={expandSidebar} theme={theme} />}
+          <div className="okp4-dataverse-portal-toolbar-logo">
+            <Okp4Logo />
+          </div>
+        </>
+      )}
+      <WalletButton isSmallScreen={isSmallScreen} label={t('toolbar.connectWallet')} />
     </div>
   )
 }
