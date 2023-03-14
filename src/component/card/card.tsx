8247c66e7d6bf0bbb183fb94eb8a027ac9665207
ErikssonJoakim <j.eriksson@okp4.com>
2023-03-14 14:37:41 +0100

    feat(card): add class name property

--- src/component/card/card.tsx
+++ src/component/card/card.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react'
 import './card.scss'
+import classNames from 'classnames'
 
 export type CardProps = {
   children: React.ReactElement
+  mainClassName?: string
 }
 
-export const Card: FC<CardProps> = ({ children }) => {
-  return <div className="okp4-dataverse-portal-card-main">{children}</div>
-}
+export const Card: FC<CardProps> = ({ children, mainClassName }) => (
+  <div className={classNames('okp4-dataverse-portal-card-main', mainClassName)}>{children}</div>
+)
