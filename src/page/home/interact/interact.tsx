ec348fbac3e9fc142f781c2e736b7e6af914f960
Anja Miscevic <anja.miscevic@myy.haaga-helia.fi>
2023-03-21 13:07:12 +0100

    refactor: update InteractCard object properties

--- src/page/home/interact/interact.tsx
+++ src/page/home/interact/interact.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import type { CardBackground } from '@/component/card/card'
+import classNames from 'classnames'
 import { Card } from '@/component/card/card'
 import './interact.scss'
 import '../i18n/index'
@@ -9,7 +10,10 @@ import '../i18n/index'
 type InteractCard = {
   action: string
   category: string
-  backgroundColor: CardBackground
+  disabled: boolean
+  style: {
+    backgroundColor: CardBackground
+  }
 }
 
 // eslint-disable-next-line max-lines-per-function
@@ -21,41 +25,54 @@ export const Interact: FC = () => {
       {
         action: t('home.blocks.interact.data-services.action'),
         category: t('home.blocks.interact.data-services.category'),
-        backgroundColor: 'primary-color-variant-1'
+        disabled: true,
+        style: {
+          backgroundColor: 'primary-color-variant-1'
+        }
       },
       {
         action: t('home.blocks.interact.knowledge.action'),
         category: t('home.blocks.interact.knowledge.category'),
-        backgroundColor: 'primary-color'
+        disabled: true,
+        style: {
+          backgroundColor: 'primary-color'
+        }
       },
       {
         action: t('home.blocks.interact.data-spaces.action'),
         category: t('home.blocks.interact.data-spaces.category'),
-        backgroundColor: 'primary-color-variant-2'
+        disabled: true,
+        style: {
+          backgroundColor: 'primary-color-variant-2'
+        }
       },
       {
         action: t('home.blocks.interact.applications.action'),
         category: t('home.blocks.interact.applications.category'),
-        backgroundColor: 'primary-color-variant-3'
+        disabled: true,
+        style: {
+          backgroundColor: 'primary-color-variant-3'
+        }
       }
     ],
     [t]
   )
+
   return (
     <>
       <h1>{t('home.blocks.interact.label')}</h1>
-      {interactCards.map(({ action, category, backgroundColor }, index) => (
+      {interactCards.map(({ action, category, disabled, style }, index) => (
         <Card
-          background={backgroundColor}
+          background={style.backgroundColor}
           key={index}
-          mainClassName="okp4-dataverse-portal-home-page-interact-card"
+          mainClassName={classNames('okp4-dataverse-portal-home-page-interact-card', { disabled })}
         >
           <div>
             <h3>{action}</h3>
             <h2>{category}</h2>
-            <div className="okp4-dataverse-portal-home-page-interact-card-disabled">
-              <h2>{t('home.blocks.interact.disabled')}</h2>
-            </div>
+            <h2 className="okp4-dataverse-portal-home-page-interact-card-disabled-text">
+              {t('home.blocks.interact.disabled')}
+            </h2>
           </div>
         </Card>
       ))}
