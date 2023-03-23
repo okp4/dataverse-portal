f91a0c9443a2778a01da6bfa242d918885c48340
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-03-23 15:53:27 +0100

    feat(ui): update dataverse content

--- src/page/dataverse/dataverse.tsx
+++ src/page/dataverse/dataverse.tsx
@@ -47,118 +47,121 @@ const filters: Filter[] = [
 const dataverseItems: DataverseItemDetails[] = [
   {
     type: 'dataspace',
-    label: 'AG Open Data Space',
-    description: 'Lorem ipsum dolor sit amet, consectetur '
-  },
-  {
-    type: 'dataset',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
-    description: 'Données publiques de météo'
-  },
-  {
-    type: 'service',
-    label: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip',
-    description: 'Données publiques de météo'
-  },
-  {
-    type: 'dataspace',
-    label:
-      'Vitae justo eget magna fermentum iaculis eu. Ullamcorper morbi tincidunt ornare massa eget egestas',
-    description: 'Elementum metus magnis amet'
-  },
-  {
-    type: 'dataset',
-    label: 'This is a title that is much more letters and words',
-    description: 'This is a description that is far too long to be written here in full.'
-  },
-  {
-    type: 'service',
-    label:
-      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    label: 'Rhizome',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'Rhizome is a data space operated by OKP4, currently under development based on OKP4 technology. Rhizome demonstrates the power of data processing and sharing, and the value we can achieve by effectively connecting different sources of open access agricultural data in different data formats. Rhizome aims to connect as much data as possible and provide valuable visuals and metrics in various agriculture-related areas, such as land use and land management, crop and livestock management, and forest resources and timber industry.'
   },
   {
-    type: 'dataspace',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
-    description: 'Commodo dictumst porttitor at.'
+    type: 'dataset',
+    label: 'ADMIN EXPRESS COG 2022 DEPARTMENT',
+    description:
+      'ADMIN EXPRESS allows cross-referencing with other data sources in order to build thematic representations of the territory according to an administrative granularity (commune, departmental district, department, region). ADMIN EXPRESS is available in a "COG" edition, in accordance with the official geographic code published each year by INSEE.'
+  },
+  {
+    type: 'service',
+    label: 'Data Connector',
+    description:
+      'This service allows you to inject data files into OpenSearch so that you can consume the knowledge created through visualization.'
   },
   {
     type: 'dataset',
     label:
-      'Maecenas ultricies mi eget mauris pharetra et. Fermentum iaculis eu non diam phasellus.',
-    description: 'Pellentesque.Ipsum odor amet'
-  },
-  {
-    type: 'service',
-    label: 'Id cursus metus aliquam eleifend',
+      'Crop and crop group reference table of the Graphic Parcel Register (Registre Parcellaire Graphique)',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
-  },
-  {
-    type: 'dataspace',
-    label: 'Maecenas ultricies mi eget mauris pharetra et. Fermentum iaculis',
-    description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'Table specific to the distribution of the Graphic Parcel Register (Registre Parcellaire Graphique)): The notion of crop group in this table does not correspond to the notion of crop group of the CAP regulation nor to that of the ISIS reference systems. In this table, each crop code is explained by a label and linked to a crop group code and its label.'
   },
   {
     type: 'dataset',
-    label: 'Sed libero enim sed faucibus turpis.',
-    description: 'Elementum metus magnis commodo dictumst'
+    label: 'ADMIN EXPRESS COG 2022 REGION',
+    description:
+      'ADMIN EXPRESS allows cross-referencing with other data sources in order to build thematic representations of the territory according to an administrative granularity (commune, departmental district, department, region). ADMIN EXPRESS is available in a "COG" edition, in accordance with the official geographic code published each year by INSEE.'
   },
   {
     type: 'service',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
+    label: 'Data Refactor',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
-  },
-  {
-    type: 'dataspace',
-    label: 'Integer feugiat scelerisque varius morbi. Odio ut enim blandit volutpat maecenas vol',
-    description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'The Data Refactor tool allows you to edit and modify a data set in order to standardize it.'
   },
   {
     type: 'dataset',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
+    label: 'FRANCE : REGION - DEPARTMENT - CITY',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'This dataset is obtained by joining 3 ADMIN EXPRESS datasets containing the different layers of the French territory: Region, Department and City.'
+  },
+  {
+    type: 'dataset',
+    label: 'ADMIN EXPRESS COG 2020 DEPARTMENT',
+    description:
+      'ADMIN EXPRESS allows cross-referencing with other data sources in order to build thematic representations of the territory according to an administrative granularity (commune, departmental district, department, region). ADMIN EXPRESS is available in a "COG" edition, in accordance with the official geographic code published each year by INSEE.'
   },
   {
     type: 'service',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
+    label: 'Data Join Tabular',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
-  },
-  {
-    type: 'dataspace',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
-    description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'The Data Join Tabular tool allows you to join two datasets containing common values to provide new information.'
   },
   {
     type: 'dataset',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
+    label: 'ADMIN EXPRESS COG 2022 CITY',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'ADMIN EXPRESS allows cross-referencing with other data sources in order to build thematic representations of the territory according to an administrative granularity (commune, departmental district, department, region). ADMIN EXPRESS is available in a "COG" edition, in accordance with the official geographic code published each year by INSEE.'
+  },
+  {
+    type: 'dataset',
+    label: 'RPG FRANCE 2020',
+    description:
+      'The graphical parcel register is a geographic database used as a reference for the instruction of Common Agricultural Policy (CAP) subsidies. The anonymized version distributed here as part of the public service for making reference data available contains the graphic data of parcels (since 2015) with their main crop. These data are produced by the Agency of Services and Payment (ASP) since 2007'
   },
   {
     type: 'service',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
-    description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
-  },
-  {
-    type: 'dataspace',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
-    description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+    label: 'Data Grouping',
+    description: 'The Data Grouping tool allows you to organize identical data into groups.'
   },
   {
     type: 'dataset',
-    label: 'Lorem ipsum odor amet, consectetuer adipiscing elit',
+    label: 'AGRESTE 2020',
     description:
-      'Elementum metus magnis commodo dictumst porttitor at eu pellentesque.Ipsum odor amet'
+      'Annual agricultural statistics, consisting of the areas, yields and production of the French territory.'
+  },
+  {
+    type: 'dataset',
+    label: 'RPG AGRESTE 2020',
+    description:
+      'Data set resulting from the join between RPG and AGRESTE data. Obtained through a sequence of data processing using the OKP4 protocol.'
+  },
+  {
+    type: 'service',
+    label: 'Storage',
+    description: 'Data storage service'
+  },
+  {
+    type: 'dataset',
+    label: 'ADMIN EXPRESS COG 2020 REGION',
+    description:
+      'ADMIN EXPRESS allows cross-referencing with other data sources in order to build thematic representations of the territory according to an administrative granularity (commune, departmental district, department, region). ADMIN EXPRESS is available in a "COG" edition, in accordance with the official geographic code published each year by INSEE.'
+  },
+  {
+    type: 'dataset',
+    label: 'Reference table of NAF Rev2 codes',
+    description:
+      'The French Nomenclature of Activities (NAF) is a nomenclature of productive economic activities, mainly developed to facilitate the organization of economic and social information.'
+  },
+  {
+    type: 'service',
+    label: 'Data Join Geospatial',
+    description: 'The Data Join Geospatial tool allows to join two georeferenced datasets.'
+  },
+  {
+    type: 'dataset',
+    label: 'BASE SIRENE - NAF',
+    description:
+      'This dataset is obtained using the join between the SIRENE database and the NAF code reference table to obtain information on the activity of establishments.'
+  },
+  {
+    type: 'dataset',
+    label: 'SIRENE database - agriculture, forestry and fishing',
+    description:
+      'This dataset refers to all the agricultural, forestry and fishing companies in France in activity. Obtained through a sequence of data processing using the OKP4 protocol.'
   }
 ]
 
