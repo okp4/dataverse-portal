942502d5310c337fff8bc81064b4ce0953ec1ecf
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-03-28 18:34:40 +0200

    feat: create ResourceDetails component

---
+++ src/component/resource-details/resourceDetails.tsx
@@ -0,0 +1,20 @@
+import './resourceDetails.scss'
+import type { FC } from 'react'
+
+type ResourceDetailsProps = {
+  icon: React.ReactNode
+  title: string
+  description: string
+}
+
+export const ResourceDetails: FC<ResourceDetailsProps> = ({ icon, title, description }) => {
+  return (
+    <div className="okp4-dataverse-portal-resource-details">
+      <div className="okp4-dataverse-portal-resource-details-icon">{icon}</div>
+      <div className="okp4-dataverse-portal-resource-details-content">
+        <h3 className="okp4-dataverse-portal-resource-details-title">{title}</h3>
+        <p className="okp4-dataverse-portal-resource-details-description">{description}</p>
+      </div>
+    </div>
+  )
+}
