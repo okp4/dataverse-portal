7f2dea74f7861348d1b39a0f0ca3909764921d1e
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-03-29 14:25:09 +0200

    refactor: rename general metadata directory

--- src/ui/page/dataverse/service/service.tsx
+++ src/ui/page/dataverse/service/service.tsx
@@ -4,8 +4,8 @@ import type { Option } from 'fp-ts/Option'
 import { match, none } from 'fp-ts/Option'
 import { getResourceDetails } from '@/ui/page/dataverse/dataverse'
 import type { DataverseItemDetails } from '@/ui/page/dataverse/dataverse'
-import type { GeneralMetadata } from '@/component/general-metadata/generalMetadata'
-import { GeneralMetadataList } from '@/component/general-metadata/generalMetadata'
+import type { GeneralMetadata } from '@/component/generalMetadata/generalMetadata'
+import { GeneralMetadataList } from '@/component/generalMetadata/generalMetadata'
 
 const serviceGeneralMetadata: GeneralMetadata[] = [
   {
