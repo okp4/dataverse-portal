c139761c028bf0b0096fe050604579759e9a627f
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-03-29 12:15:34 +0200

    feat: create a list of general metadata items

--- src/component/general-metadata/generalMetadata.tsx
+++ src/component/general-metadata/generalMetadata.tsx
@@ -1,20 +1,43 @@
 import './generalMetadata.scss'
 import type { FC } from 'react'
+import type { IconName } from '@/component/icon/icon'
+import { Icon } from '@/component/icon/icon'
 
-type GeneralMetadataProps = {
-  icon: JSX.Element
+export type GeneralMetadata = {
+  iconName: IconName
   title: string
   description: string
 }
 
-export const GeneralMetadata: FC<GeneralMetadataProps> = ({ icon, title, description }) => {
+const GeneralMetadataItem: FC<GeneralMetadata> = ({ iconName, title, description }) => {
   return (
-    <div className="okp4-dataverse-portal-resource-details">
-      <div className="okp4-dataverse-portal-resource-details-icon">{icon}</div>
-      <div className="okp4-dataverse-portal-resource-details-content">
-        <h3 className="okp4-dataverse-portal-resource-details-title">{title}</h3>
-        <p className="okp4-dataverse-portal-resource-details-description">{description}</p>
+    <div className="okp4-dataverse-portal-general-metadata">
+      <div className="okp4-dataverse-portal-general-metadata-icon">
+        <Icon name={iconName} />
       </div>
+      <div className="okp4-dataverse-portal-general-metadata-content">
+        <h3 className="okp4-dataverse-portal-general-metadata-title">{title}</h3>
+        <p className="okp4-dataverse-portal-general-metadata-description">{description}</p>
+      </div>
+    </div>
+  )
+}
+
+type GeneralMetadataListProps = {
+  metadata: GeneralMetadata[]
+}
+
+export const GeneralMetadataList: FC<GeneralMetadataListProps> = ({ metadata }) => {
+  return (
+    <div className="okp4-dataverse-portal-general-metadata-list">
+      {metadata.map(({ description, iconName, title }) => (
+        <GeneralMetadataItem
+          description={description}
+          iconName={iconName}
+          key={title}
+          title={title}
+        />
+      ))}
     </div>
   )
 }
