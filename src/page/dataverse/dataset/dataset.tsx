7b8e7e82af9d026a18a5e8c90a0a3b6145e1e68c
Geoffrey <geoffrey.picardpro@gmail.com>
2023-03-29 11:55:07 +0200

    feat: add dataset description

--- src/page/dataverse/dataset/dataset.tsx
+++ src/page/dataverse/dataset/dataset.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import type { Option } from 'fp-ts/Option'
 import { match, none } from 'fp-ts/Option'
-import { getResourceDetails } from '@/page/dataverse/dataverse'
-import type { DataverseItemDetails } from '@/page/dataverse/dataverse'
+import { getResourceDetails } from '../dataverse'
+import type { DataverseItemDetails } from '../dataverse'
+import '../index.scss'
 
 const Dataset = (): JSX.Element => {
   const { id } = useParams<string>()
@@ -15,7 +16,14 @@ const Dataset = (): JSX.Element => {
 
   return match(
     () => <p>Dataset not found</p>,
-    (dataset: DataverseItemDetails) => <p>{dataset.label}</p>
+    (dataset: DataverseItemDetails) =>
+      <div className='okp4-dataverse-portal-dataverse-index-description-main'>
+        <div className='okp4-dataverse-portal-dataverse-index-description-wrapper'>
+          <div className='okp4-dataverse-portal-dataverse-index-description-category'>{dataset.type}</div>
+          <div className='okp4-dataverse-portal-dataverse-index-description-label'>{dataset.label}</div>
+          <div className='okp4-dataverse-portal-dataverse-index-description-text'>{dataset.description}</div>
+        </div>
+      </div>
   )(dataset)
 }
 
