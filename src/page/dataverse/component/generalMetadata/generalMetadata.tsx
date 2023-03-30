105a0310dc1bc2dd71991350f5582cb83a2d828f
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-03-30 13:58:04 +0200

    refactor: generalMetadata is not aware of title context

--- src/page/dataverse/component/generalMetadata/generalMetadata.tsx
+++ src/page/dataverse/component/generalMetadata/generalMetadata.tsx
@@ -6,52 +6,42 @@ import { Icon } from '@/component/icon/icon'
 import './generalMetadata.scss'
 import './i18n/index'
 
-export type GeneralMetadataTitle =
-  | 'format'
-  | 'topic'
-  | 'license'
-  | 'geographicalCoverage'
-  | 'temporalCoverage'
-  | 'category'
-
 export type GeneralMetadata = {
   iconName: IconName
-  title: GeneralMetadataTitle
+  title: string
   description: string
 }
 
-const GeneralMetadataItem: FC<GeneralMetadata> = memo(({ iconName, title, description }) => {
-  const { t } = useTranslation('generalMetadata')
-
-  return (
-    <div className="okp4-dataverse-portal-general-metadata-item-main">
-      <div className="okp4-dataverse-portal-general-metadata-item-icon">
-        <Icon name={iconName} />
-      </div>
-      <div className="okp4-dataverse-portal-general-metadata-item-content">
-        <h3 className="okp4-dataverse-portal-general-metadata-item-title">
-          {t(`generalMetadata.${title}`)}
-        </h3>
-        <p className="okp4-dataverse-portal-general-metadata-item-description">{description}</p>
-      </div>
+const GeneralMetadataItem: FC<GeneralMetadata> = memo(({ iconName, title, description }) => (
+  <div className="okp4-dataverse-portal-general-metadata-item-main">
+    <div className="okp4-dataverse-portal-general-metadata-item-icon">
+      <Icon name={iconName} />
     </div>
-  )
-})
+    <div className="okp4-dataverse-portal-general-metadata-item-content">
+      <h3 className="okp4-dataverse-portal-general-metadata-item-title">{title}</h3>
+      <p className="okp4-dataverse-portal-general-metadata-item-description">{description}</p>
+    </div>
+  </div>
+))
 GeneralMetadataItem.displayName = 'GeneralMetadataItem'
 
 type GeneralMetadataListProps = {
   metadata: GeneralMetadata[]
 }
 
-export const GeneralMetadataList: FC<GeneralMetadataListProps> = ({ metadata }) => (
-  <div className="okp4-dataverse-portal-general-metadata-list-main">
-    {metadata.map(({ description, iconName, title }) => (
-      <GeneralMetadataItem
-        description={description}
-        iconName={iconName}
-        key={title}
-        title={title}
-      />
-    ))}
-  </div>
-)
+export const GeneralMetadataList: FC<GeneralMetadataListProps> = ({ metadata }) => {
+  const { t } = useTranslation('generalMetadata')
+
+  return (
+    <div className="okp4-dataverse-portal-general-metadata-list-main">
+      {metadata.map(({ description, iconName, title }) => (
+        <GeneralMetadataItem
+          description={description}
+          iconName={iconName}
+          key={title}
+          title={t(`generalMetadata.${title}`)}
+        />
+      ))}
+    </div>
+  )
+}
