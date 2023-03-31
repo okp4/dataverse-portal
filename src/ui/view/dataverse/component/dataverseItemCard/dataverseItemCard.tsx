221660cec14072676c7285f62cc74cb74fe1a879
Geoffrey <geoffrey.picardpro@gmail.com>
2023-03-31 13:14:36 +0200

    refactor: replace tag component by div for card category

--- src/ui/view/dataverse/component/dataverseItemCard/dataverseItemCard.tsx
+++ src/ui/view/dataverse/component/dataverseItemCard/dataverseItemCard.tsx
@@ -1,13 +1,12 @@
 import type { FC } from 'react'
 import { useCallback } from 'react'
-import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import { Card } from '@/ui/component/card/card'
 import { Button } from '@/ui/component/button/button'
-import { Tag } from '@/ui/component/tag/tag'
-import type { ColorVariant } from '@/ui/component/tag/tag'
 import './dataverseItemCard.scss'
+import { useTranslation } from 'react-i18next'
+import classNames from 'classnames'
 
 type DataverseItem = 'dataspace' | 'dataset' | 'service'
 
@@ -16,13 +15,16 @@ export type DataverseItemCardProps = {
   type: DataverseItem
   label: string
   description: string
+  tags?: Array<string>
 }
 
 export const DataverseItemCard: FC<DataverseItemCardProps> = ({ id, type, label, description }) => {
   const navigate = useNavigate()
   const { t } = useTranslation('common')
 
-  const renderTagColor = useCallback((type: DataverseItem): ColorVariant => {
+  type ColorVariant = 'primary-color' | 'primary-color-variant-3' | 'primary-color-variant-4'
+
+  const renderItemTypeColor = useCallback((type: DataverseItem): ColorVariant => {
     switch (type) {
       case 'service':
         return 'primary-color'
@@ -45,8 +47,8 @@ ${description}`,
 
   return (
     <Card>
-      <div className="okp4-dataverse-portal-dataverse-item-card-main">
-        <Tag colorVariant={renderTagColor(type)} label={t(`resources.${type}`)} />
+      <div className="okp4-dataverse-portal-dataverse-card-main">
+        <div className={classNames('okp4-dataverse-portal-dataverse-item-type', renderItemTypeColor(type))}>{t(`resources.${type}`)}</div>
         <div className="okp4-dataverse-portal-dataverse-item-card-content">
           <div className="okp4-dataverse-portal-dataverse-description">
             <ReactMarkdown>{renderItemContent(label, description)}</ReactMarkdown>
