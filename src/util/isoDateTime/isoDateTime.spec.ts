59e7094ca85089c1491370388e951399d5494098
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-03 18:05:59 +0200

    test: update tests according to the latest isoDateTime API

--- src/util/isoDateTime/isoDateTime.spec.ts
+++ src/util/isoDateTime/isoDateTime.spec.ts
@@ -1,18 +1,24 @@
 import { convertToLocalizedYearIfISODateTime } from './isoDateTime'
+import * as E from 'fp-ts/Either'
+
+const mockedResources = {
+  en: {},
+  fr: {}
+}
 
 describe('convertToLocalizedYearIfISODateTime', () => {
   test.each([
-    ['2023-01-01T00:00:00', 'en', '2023'],
-    ['2023-01-01T00:00:00.000', 'en', '2023'],
-    ['2023-01-01T00:00:00.000Z', 'en', '2023'],
-    ['002023-01-01T00:00:00.000+01:00', 'en', '002023-01-01T00:00:00.000+01:00'],
-    ['2023-01-01T00:00:00', 'fr', '2023'],
-    ['2023-01-01T00:00:00.000Z', 'fr', '2023'],
-    ['Not an ISO DateTime', 'en', 'Not an ISO DateTime']
+    ['2023-01-01T00:00:00', 'en', E.right('2023')],
+    ['2023-01-01T00:00:00.000', 'en', E.right('2023')],
+    ['2023-01-01T00:00:00.000Z', 'en', E.right('2023')],
+    ['002023-01-01T00:00:00.000+01:00', 'en', E.left(new Error('Invalid date'))],
+    ['2023-01-01T00:00:00', 'fr', E.right('2023')],
+    ['2023-01-01T00:00:00.000Z', 'fr', E.right('2023')],
+    ['Not an ISO DateTime', 'en', E.left(new Error('Invalid date'))]
   ])(
     'converts input %s with locale %s to %s',
-    (input: string, locale: string, expected: string) => {
-      expect(convertToLocalizedYearIfISODateTime(input, locale)).toBe(expected)
+    (input: string, locale: string, expected: E.Either<Error, string>) => {
+      expect(convertToLocalizedYearIfISODateTime(input, locale, mockedResources)).toEqual(expected)
     }
   )
 
@@ -20,9 +26,9 @@ describe('convertToLocalizedYearIfISODateTime', () => {
   test('converts input with timezone offset to the correct year', () => {
     const input = '2023-01-01T00:00:00.000+01:00'
     const lng = 'en'
-    const result = convertToLocalizedYearIfISODateTime(input, lng)
+    const result = convertToLocalizedYearIfISODateTime(input, lng, mockedResources)
 
     // Check if the result is either '2022' or '2023', depending on the system's timezone
-    expect(['2022', '2023']).toContain(result)
+    expect([E.right('2022'), E.right('2023')]).toContainEqual(result)
   })
 })
