b7ca10c110c9363af5c477f47493d3d09446bf69
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-03 17:16:07 +0200

    feat: apply ISOstring conversion on temporal coverage value

--- src/ui/view/dataverse/component/generalMetadata/generalMetadata.tsx
+++ src/ui/view/dataverse/component/generalMetadata/generalMetadata.tsx
@@ -1,5 +1,6 @@
+import * as E from 'fp-ts/Either'
 import type { FC } from 'react'
-import { memo } from 'react'
+import { useCallback, memo } from 'react'
 import { useTranslation } from 'react-i18next'
 import type { IconName } from '@/ui/component/icon/icon'
 import { Icon } from '@/ui/component/icon/icon'
@@ -7,6 +8,7 @@ import { activeLanguageWithDefault } from '@/ui/languages/languages'
 import { convertToLocalizedYearIfISODateTime } from '@/util/isoDateTime/isoDateTime'
 import './generalMetadata.scss'
 import './i18n/index'
+import { pipe } from 'fp-ts/lib/function'
 
 export type MetadataProperty = {
   property: string
@@ -37,6 +39,36 @@ export const GeneralMetadataList: FC<GeneralMetadataListProps> = ({ metadata }) 
   const { t } = useTranslation('generalMetadata')
   const { lng } = activeLanguageWithDefault()
 
+  const handleLocalizedYearOrError = useCallback(
+    (fallback: string) =>
+      E.fold(
+        (error: Error) => {
+          console.error(error.message)
+          return fallback
+        },
+        (localizedYear: string) => localizedYear
+      ),
+    []
+  )
+
+  const convertValueToLocalizedYearIfISODateTime = useCallback(
+    (value: string) =>
+      pipe(
+        convertToLocalizedYearIfISODateTime(value, lng),
+        handleLocalizedYearOrError(t('generalMetadata.invalidDate'))
+      ),
+    [handleLocalizedYearOrError, lng, t]
+  )
+
+  const displayValue = useCallback(
+    (value: string, property: string) => {
+      return property === 'temporalCoverage'
+        ? convertValueToLocalizedYearIfISODateTime(value)
+        : value
+    },
+    [convertValueToLocalizedYearIfISODateTime]
+  )
+
   return (
     <div className="okp4-dataverse-portal-general-metadata-list-main">
       {metadata.map(({ value, iconName, property }) => (
@@ -44,7 +76,7 @@ export const GeneralMetadataList: FC<GeneralMetadataListProps> = ({ metadata }) 
           iconName={iconName}
           key={property}
           property={t(`generalMetadata.${property}`)}
-          value={convertToLocalizedYearIfISODateTime(value, lng)}
+          value={displayValue(value, property)}
         />
       ))}
     </div>
