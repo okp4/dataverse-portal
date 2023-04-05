0a185d3f639897cbb3abef89b7dcc7d25468ecbd
ErikssonJoakim <j.eriksson@okp4.com>
2023-04-05 18:46:21 +0200

    feat(dataverse): add dataspace dataverse item stat cards

--- src/ui/view/dataverse/component/pageTemplate/pageTemplate.tsx
+++ src/ui/view/dataverse/component/pageTemplate/pageTemplate.tsx
@@ -1,17 +1,19 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback } from 'react'
 import type { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { pipe } from 'fp-ts/lib/function'
 import * as A from 'fp-ts/Array'
+import { useTranslation } from 'react-i18next'
 import type { DataverseItemDetails } from '@/ui/page/dataverse/dataverse'
 import type { ItemGeneralMetadata } from '@/ui/view/dataverse/types'
 import { GeneralMetadataList } from '@/ui/view/dataverse/component/generalMetadata/generalMetadata'
 import ItemOverview from '@/ui/view/dataverse/component/itemOverview/itemOverview'
-import './pageTemplate.scss'
 import { GovernanceDescription } from '@/ui/view/dataverse/component/governanceDescription/governanceDescription'
 import { isDataSpace } from '@/ui/page/dataverse/dataspace/dataspace'
 import { Icon } from '@/ui/component/icon/icon'
-import { useTranslation } from 'react-i18next'
+import { DataverseItemStatCard } from '@/ui/view/dataverse/component/dataverseItemStatCard/dataverseItemStatCard'
+import './pageTemplate.scss'
+import './i18n/index'
 
 type PageTemplateProps = {
   data: DataverseItemDetails
@@ -28,20 +30,18 @@ const isGeneralMetadata = (
 ): metadata is Omit<ItemGeneralMetadata, 'value'> & { value: string } =>
   metadata.property !== 'tags'
 
+const tags = (metadata: ItemGeneralMetadata[]): string[] =>
+  pipe(
+    metadata,
+    A.filter(isTagsMetadata),
+    A.chain(metadata => metadata.value)
+  )
+
 const PageTemplate: FC<PageTemplateProps> = ({ data, metadata }): JSX.Element => {
-  const { t } = useTranslation('common')
+  const { t } = useTranslation(['pageTemplate', 'common'])
   const navigate = useNavigate()
   const backToDataverse = useCallback((): void => navigate('/dataverse'), [navigate])
 
-  const tags = useMemo(
-    () =>
-      pipe(
-        metadata,
-        A.filter(isTagsMetadata),
-        A.chain(metadata => metadata.value)
-      ),
-    [metadata]
-  )
   const generalMetadata = pipe(metadata, A.filter(isGeneralMetadata))
 
   return (
@@ -55,12 +55,29 @@ const PageTemplate: FC<PageTemplateProps> = ({ data, metadata }): JSX.Element =>
       <div className="okp4-dataverse-portal-dataverse-page-template-left-side-wrapper">
         <ItemOverview
           description={data.description}
-          tags={tags}
+          tags={tags(metadata)}
           title={data.label}
           type={data.type}
         />
         <GeneralMetadataList metadata={generalMetadata} />
         {isDataSpace(data) && <GovernanceDescription description={data.governance.description} />}
+      </div>
+      <div className="okp4-dataverse-portal-dataverse-page-template-right-side-wrapper">
+        {isDataSpace(data) && (
+          <>
+            <h2>{t('inDataspace')}</h2>
+            <div className="okp4-dataverse-portal-dataverse-page-template-dataverse-item-stat-cards">
+              {data.resources.map(({ type, amount, lastUpdated }) => (
+                <DataverseItemStatCard
+                  amount={amount}
+                  key={type}
+                  lastUpdated={lastUpdated}
+                  type={type}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
