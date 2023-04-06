cb30b7a30c85741f99984bd35123b38d1e826ac2
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-06 13:15:10 +0200

    feat(i18n): add translations for item type

--- src/ui/view/dataverse/component/itemOverview/itemOverview.tsx
+++ src/ui/view/dataverse/component/itemOverview/itemOverview.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react'
 import { Tags } from '@/ui/view/dataverse/component//tags/tags'
 import './itemOverview.scss'
+import { useTranslation } from 'react-i18next'
 
 type ItemOverviewProps = {
   type: string
@@ -9,13 +10,18 @@ type ItemOverviewProps = {
   tags: string[]
 }
 
-const ItemOverview: FC<ItemOverviewProps> = ({ type, title, description, tags }): JSX.Element => (
-  <div className="okp4-dataverse-portal-dataverse-item-overview-main">
-    <div className="okp4-dataverse-portal-dataverse-item-overview-type">{type}</div>
-    <h2 className="okp4-dataverse-portal-dataverse-item-overview-title">{title}</h2>
-    <p className="okp4-dataverse-portal-dataverse-item-overview-description">{description}</p>
-    {tags.length > 0 && <Tags tags={tags} />}
-  </div>
-)
+const ItemOverview: FC<ItemOverviewProps> = ({ type, title, description, tags }): JSX.Element => {
+  const { t } = useTranslation('common')
+  return (
+    <div className="okp4-dataverse-portal-dataverse-item-overview-main">
+      <div className="okp4-dataverse-portal-dataverse-item-overview-type">
+        {t(`resources.${type}`)}
+      </div>
+      <h2 className="okp4-dataverse-portal-dataverse-item-overview-title">{title}</h2>
+      <p className="okp4-dataverse-portal-dataverse-item-overview-description">{description}</p>
+      {tags.length > 0 && <Tags tags={tags} />}
+    </div>
+  )
+}
 
 export default ItemOverview
