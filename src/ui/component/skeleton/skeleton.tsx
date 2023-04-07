fcee874bba45a6d49cc0afbc7589a8bd90a71862
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-07 12:35:42 +0200

    feat: allow to style skeleton and add several ones	 using count

--- src/ui/component/skeleton/skeleton.tsx
+++ src/ui/component/skeleton/skeleton.tsx
@@ -1,24 +1,52 @@
-import type { FC } from 'react'
+import type { CSSProperties, FC } from 'react'
 import classNames from 'classnames'
 import './skeleton.scss'
 
 type SkeletonProps = {
-  width?: number
-  height?: number
-  fontSize?: number
+  width?: number | string
+  height?: number | string
+  fontSize?: number | string
   variant?: 'text' | 'rectangular' | 'rounded' | 'circular'
+  className?: string
+  style?: CSSProperties
+  count?: number
 }
 
-export const Skeleton: FC<SkeletonProps> = ({ width, height, variant = 'text', fontSize }) => {
-  const classSkeleton = classNames('okp4-dataverse-portal-skeleton', variant)
-  return (
-    <div
-      className={classSkeleton}
-      style={{
-        width: width ? `${width}px` : '100%',
-        height: height ? `${height}px` : '100%',
-        fontSize: fontSize ? `${fontSize}px` : '100%'
-      }}
-    ></div>
-  )
+export const Skeleton: FC<SkeletonProps> = ({
+  width,
+  height,
+  variant = 'text',
+  fontSize,
+  className,
+  style,
+  count = 1
+}) => {
+  const isRounded = variant === 'rounded'
+
+  if (isRounded && !height) {
+    throw new Error(
+      "A height must be provided when using the 'rounded' variant of Skeleton component."
+    )
+  }
+
+  const borderRadius =
+    isRounded &&
+    (typeof height === 'number' ? `${Math.round(height * 0.5)}px` : `calc(${height} * 0.5)`)
+
+  const classSkeleton = classNames('okp4-dataverse-portal-skeleton', variant, className)
+
+  const mergedStyle: CSSProperties = {
+    width: width ? (typeof width === 'number' ? `${width}px` : width) : '100%',
+    ...(height ? { height: typeof height === 'number' ? `${height}px` : height } : {}),
+    ...(fontSize ? { fontSize: typeof fontSize === 'number' ? `${fontSize}px` : fontSize } : {}),
+    ...(borderRadius ? { borderRadius } : {}),
+    ...style
+  }
+
+  const renderSkeletons = (): JSX.Element[] =>
+    Array(count)
+      .fill(null)
+      .map((_, i) => <div className={classSkeleton} key={i} style={mergedStyle}></div>)
+
+  return <>{renderSkeletons()}</>
 }
