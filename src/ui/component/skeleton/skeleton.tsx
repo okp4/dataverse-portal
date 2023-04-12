e14fae3a66ee61ca2303ead9af1d0c4e4fbf7cb4
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-12 08:44:24 +0200

    refactor: remove styles, fontSize and count properties

--- src/ui/component/skeleton/skeleton.tsx
+++ src/ui/component/skeleton/skeleton.tsx
@@ -5,48 +5,31 @@ import './skeleton.scss'
 type SkeletonProps = {
   width?: number | string
   height?: number | string
-  fontSize?: number | string
   variant?: 'text' | 'rectangular' | 'rounded' | 'circular'
-  className?: string
-  style?: CSSProperties
-  count?: number
 }
 
-export const Skeleton: FC<SkeletonProps> = ({
-  width,
-  height,
-  variant = 'text',
-  fontSize,
-  className,
-  style,
-  count = 1
-}) => {
-  const isRounded = variant === 'rounded'
+export const Skeleton: FC<SkeletonProps> = ({ width, height, variant = 'text' }) => {
+  const mergedStyle: CSSProperties = {
+    width: width ? (typeof width === 'number' ? `${width}px` : width) : '100%',
+    ...(height ? { height: typeof height === 'number' ? `${height}px` : height } : {})
+  }
 
+  const isRounded = variant === 'rounded'
   if (isRounded && !height) {
-    throw new Error(
+    console.error(
       "A height must be provided when using the 'rounded' variant of Skeleton component."
     )
   }
-
-  const borderRadius =
-    isRounded &&
-    (typeof height === 'number' ? `${Math.round(height * 0.5)}px` : `calc(${height} * 0.5)`)
-
-  const classSkeleton = classNames('okp4-dataverse-portal-skeleton', variant, className)
-
-  const mergedStyle: CSSProperties = {
-    width: width ? (typeof width === 'number' ? `${width}px` : width) : '100%',
-    ...(height ? { height: typeof height === 'number' ? `${height}px` : height } : {}),
-    ...(fontSize ? { fontSize: typeof fontSize === 'number' ? `${fontSize}px` : fontSize } : {}),
-    ...(borderRadius ? { borderRadius } : {}),
-    ...style
+  if (isRounded && height) {
+    const borderRadius =
+      typeof height === 'number' ? `${Math.round(height * 0.5)}px` : `calc(${height} * 0.5)`
+    mergedStyle.borderRadius = borderRadius
   }
 
-  const renderSkeletons = (): JSX.Element[] =>
-    Array(count)
-      .fill(null)
-      .map((_, i) => <div className={classSkeleton} key={i} style={mergedStyle}></div>)
-
-  return <>{renderSkeletons()}</>
+  return (
+    <div
+      className={classNames('okp4-dataverse-portal-skeleton', variant)}
+      style={mergedStyle}
+    ></div>
+  )
 }
