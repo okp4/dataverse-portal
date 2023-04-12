3090e5e9ce802aefe4c5f899d5e6773a98fb2c75
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-12 14:11:41 +0200

    refactor: remove border radius computation and fallback

--- src/ui/component/skeleton/skeleton.tsx
+++ src/ui/component/skeleton/skeleton.tsx
@@ -14,18 +14,6 @@ export const Skeleton: FC<SkeletonProps> = ({ width, height, variant = 'text' })
     ...(height ? { height: typeof height === 'number' ? `${height}px` : height } : {})
   }
 
-  const isRounded = variant === 'rounded'
-  if (isRounded && !height) {
-    console.error(
-      "A height must be provided when using the 'rounded' variant of Skeleton component."
-    )
-  }
-  if (isRounded && height) {
-    const borderRadius =
-      typeof height === 'number' ? `${Math.round(height * 0.5)}px` : `calc(${height} * 0.5)`
-    mergedStyle.borderRadius = borderRadius
-  }
-
   return (
     <div
       className={classNames('okp4-dataverse-portal-skeleton', variant)}
