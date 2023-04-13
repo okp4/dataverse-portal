4dd46bbed17047d9cdce9cc8f2f20e9c53435f70
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-13 11:05:04 +0200

    refactor: use i18next built-in fallback for missing translations

--- src/ui/view/dataverse/component/generalMetadata/generalMetadataValue.tsx
+++ src/ui/view/dataverse/component/generalMetadata/generalMetadataValue.tsx
@@ -1,7 +1,4 @@
 import { useTranslation } from 'react-i18next'
-import * as E from 'fp-ts/Either'
-import { pipe } from 'fp-ts/lib/function'
-import { fromPredicate } from 'fp-ts/lib/Either'
 import type { ItemGeneralMetadata } from '@/ui/view/dataverse/types'
 import './generalMetadata.scss'
 import './i18n/index'
@@ -22,23 +19,11 @@ type MetadataValueProps = {
 
 export const GeneralMetadataValue = ({ metadata }: MetadataValueProps): JSX.Element | null => {
   const namespace = 'generalMetadata'
-  const { t, i18n } = useTranslation(namespace)
+  const { t } = useTranslation(namespace)
   const { value, property } = metadata
+
   if (isGeneralMetadata(metadata)) {
-    const translationKeyExists = i18n.exists(`${namespace}.topic.value.${value}`, {
-      ns: namespace,
-      lng: i18n.language
-    })
-    const translatedValue = pipe(
-      value,
-      fromPredicate(
-        () => translationKeyExists,
-        () => value
-      ),
-      E.map(val => t(`${namespace}.${property}.value.${val}`)),
-      E.getOrElse(() => value)
-    )
-    return <span>{translatedValue}</span>
+    return <span>{t(`${namespace}.${property}.value.${value}`, `${value}`)}</span>
   }
 
   if (isPeriodGeneralMetadata(metadata)) {
