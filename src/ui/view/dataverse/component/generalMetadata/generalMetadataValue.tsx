ac9d5e8508917b8d5d8a985c966880086e9919ec
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-13 10:23:14 +0200

    fix: date interval reversed props

--- src/ui/view/dataverse/component/generalMetadata/generalMetadataValue.tsx
+++ src/ui/view/dataverse/component/generalMetadata/generalMetadataValue.tsx
@@ -43,7 +43,7 @@ export const GeneralMetadataValue = ({ metadata }: MetadataValueProps): JSX.Elem
 
   if (isPeriodGeneralMetadata(metadata)) {
     const [startDateValue, endDateValue] = metadata.value
-    return <DateInterval endDate={startDateValue} startDate={endDateValue} />
+    return <DateInterval endDate={endDateValue} startDate={startDateValue} />
   }
 
   console.warn('Metadata is not of type ItemGeneralMetadata')
