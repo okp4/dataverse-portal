761cb05a1d588436ab2f891406fa71f707c653a2
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-04-14 16:20:19 +0200

    feat(infra): add sparql dto

---
+++ src/infra/dataverse/sparql/dto.ts
@@ -0,0 +1,22 @@
+export type SparqlResult = {
+  results: {
+    bindings: SparqlBinding[]
+  }
+}
+
+type SparqlBindingProperty = {
+  type: string
+  value: string
+}
+
+export type SparqlBinding = {
+  id: SparqlBindingProperty
+  metadata: SparqlBindingProperty
+  type: SparqlBindingProperty
+  title: SparqlBindingProperty & {
+    'xml:lang': string
+  }
+  description: SparqlBindingProperty & {
+    'xml:lang': string
+  }
+}
