8651496956dd8be7866b5847f894a26ca09285b1
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-19 11:41:02 +0200

    refactor: update mocked governance data
    articles might contain no paragraphs

--- src/ui/page/dataverse/dataspace/governance/mockedData.ts
+++ src/ui/page/dataverse/dataspace/governance/mockedData.ts
@@ -16,6 +16,9 @@ type Numbered = {
 type NonEmptyArray<T> = [T, ...T[]]
 
 type Container<T> = {
+  contains: T[]
+}
+type NonEmptyContainer<T> = {
   contains: NonEmptyArray<T>
 }
 
@@ -30,38 +33,38 @@ type ParagraphDTO = ParagraphDTOWithNumber
 
 type ArticleDTO = DescriptedDTOWithNumber & Container<ParagraphDTO>
 
-export type SubSectionDTO = DescriptedDTOWithNumber & Container<ArticleDTO>
+export type SubSectionDTO = DescriptedDTOWithNumber & NonEmptyContainer<ArticleDTO>
 
-export type SectionDTO = DescriptedDTOWithNumber & Container<SubSectionDTO>
+export type SectionDTO = DescriptedDTOWithNumber & NonEmptyContainer<SubSectionDTO>
 
-type Chapter = DescriptedDTO & Container<SectionDTO>
+type Chapter = DescriptedDTO & NonEmptyContainer<SectionDTO>
 
 export const mockedGovernanceChapter: Chapter = {
-  id: 'chapter1',
+  id: '1',
   title: 'Chapter 1',
   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
   contains: [
     {
-      id: 'section1',
+      id: '2',
       title: 'Identity Management',
       description:
         'It refers to the processes and tools used to manage and secure the access and use of data within an Data Space.',
       number: '1.1',
       contains: [
         {
-          id: 'subsection1',
+          id: '3',
           title: 'Subsection title',
           number: '1.1.1',
           contains: [
             {
               title: 'Condition: Only Decentralized Identifiers (DID) are accepted',
-              id: 'article1',
+              id: '4',
               number: '1.1.1.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '5',
                   title: 'Verification method',
-                  description: "Must be 'KEY'",
+                  description: 'Must be ‘KEY”',
                   number: '1.1.1.1.1'
                 }
               ]
@@ -71,24 +74,24 @@ export const mockedGovernanceChapter: Chapter = {
       ]
     },
     {
-      id: 'section2',
+      id: '6',
       title: 'Governance Model',
       description:
         'It refers to the framework of policies, processes, and procedures that govern the acquisition, management, and use of data within an organization.',
       number: '1.2',
       contains: [
         {
-          id: 'subsection1',
+          id: '7',
           title: 'Create the rules',
           number: '1.2.1',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '8',
               number: '1.2.1.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '9',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -99,17 +102,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection2',
-          title: 'Update the rules',
+          id: '10',
+          title: 'Edit rules',
           number: '1.2.2',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '11',
               number: '1.2.2.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '12',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -120,20 +123,20 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection3',
-          title: 'Delete the rules',
+          id: '13',
+          title: 'Remove rules',
           description:
             'Process of removing or revoking established policies and procedures governing the management and use of data within an Data Space.',
           number: '1.2.3',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '14',
               description: 'Keys with the permissions to delete rules related to the Data Space.',
               number: '1.2.3.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '15',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -146,154 +149,80 @@ export const mockedGovernanceChapter: Chapter = {
       ]
     },
     {
-      id: 'section3',
+      id: '16',
       title: 'Datasets Management',
-      description:
-        'The management of datasets in a Data Space refers to the process of reference, modify metadatas, dereference, and download datasets.',
       number: '1.3',
       contains: [
         {
-          id: 'subsection1',
+          id: '17',
           title: 'Reference dataset',
-          description:
-            'Make the dataset enable in the dataverse so that it can be seen and used by users in a Data Space.',
           number: '1.3.1',
           contains: [
             {
               title: 'Condition: Title',
-              description: 'Keys with the permissions to delete rules related to the Data Space.',
-              id: 'article1',
+              id: '18',
               number: '1.3.1.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '19',
                   title: 'Topic',
                   description: 'Agriculture or food',
                   number: '1.3.1.1.1'
                 },
                 {
-                  id: 'paragraph2',
+                  id: '20',
                   title: 'Size',
                   description: 'Smaller than 5 Go',
                   number: '1.3.1.1.2'
                 },
                 {
-                  id: 'paragraph3',
+                  id: '21',
                   title: 'Geographical Coverage',
                   description: 'Metropolitan France or French Overseas Territories or Europe',
                   number: '1.3.1.1.3'
                 },
                 {
-                  id: 'paragraph4',
+                  id: '22',
                   title: 'Authorship',
                   description: 'Must be specified',
                   number: '1.3.1.1.4'
                 },
                 {
-                  id: 'paragraph5',
+                  id: '23',
                   title: 'Metadata',
                   description: 'Must be completed',
                   number: '1.3.1.1.5'
                 },
                 {
-                  id: 'paragraph6',
+                  id: '24',
                   title: 'Licence',
                   description:
                     'Etalab 2.0, OGL, CC-BY 2.0, ODC-BY or ODbl 1.0, Licence 3, Licence 4, Licence 5, Licence 6, Licence 7, Licence 8, Licence 9, Licence 10, Licence 11, Licence 12 Licence 13, Licence 14, Licence 15, Licence 16, Licence 17',
                   number: '1.3.1.1.7'
                 },
                 {
-                  id: 'paragraph7',
+                  id: '25',
                   title: 'Users',
                   description:
-                    'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
+                    'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575656 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575659 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575660 did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575662',
                   number: '1.3.1.1.6'
-                }
-              ]
-            },
-            {
-              title: 'To do',
-              id: 'article2',
-              number: '1.3.1.2',
-              contains: [
-                {
-                  id: 'paragraph1',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.2.1'
-                },
-                {
-                  id: 'paragraph2',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.2.2'
-                },
-                {
-                  id: 'paragraph3',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.2.3'
-                },
-                {
-                  id: 'paragraph4',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.2.4'
-                }
-              ]
-            },
-            {
-              title: 'To do',
-              id: 'article3',
-              number: '1.3.1.3',
-              contains: [
-                {
-                  id: 'paragraph2',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.2.2'
-                }
-              ]
-            },
-            {
-              title: 'To do',
-              id: 'article1',
-              number: '1.3.1.4',
-              contains: [
-                {
-                  id: 'paragraph1',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.4.1'
-                },
-                {
-                  id: 'paragraph2',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.4.2'
-                },
-                {
-                  id: 'paragraph3',
-                  title: 'To do',
-                  description: 'To do',
-                  number: '1.3.1.4.3'
                 }
               ]
             }
           ]
         },
         {
-          id: 'subsection2',
+          id: '37',
           title: 'Create metadata',
           number: '1.3.2',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '38',
               number: '1.3.2.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '39',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -304,19 +233,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection3',
-          title: 'Update metadata',
+          id: '40',
+          title: 'Edit metadata',
           number: '1.3.3',
           contains: [
             {
               title: 'Condition: Title',
-              description:
-                'Keys with the permissions to update metadatas of datasets belonging to the Data Space.',
-              id: 'article1',
+              id: '41',
               number: '1.3.3.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '42',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -327,17 +254,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection4',
-          title: 'Delete metadata',
+          id: '43',
+          title: 'Remove metadata',
           number: '1.3.4',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '44',
               number: '1.3.4.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '45',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -348,17 +275,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection5',
-          title: 'Dereference metadata',
+          id: '46',
+          title: 'Dereference dataset',
           number: '1.3.5',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '47',
               number: '1.3.5.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '48',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -369,17 +296,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection6',
-          title: 'Download metadata',
+          id: '49',
+          title: 'Download dataset',
           number: '1.3.6',
           contains: [
             {
               title: 'Condition: Title',
-              id: 'article1',
+              id: '50',
               number: '1.3.6.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '51',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -392,22 +319,22 @@ export const mockedGovernanceChapter: Chapter = {
       ]
     },
     {
-      id: 'section4',
+      id: '52',
       title: 'Services Management',
       number: '1.4',
       contains: [
         {
-          id: 'subsection1',
-          title: 'Reference dataset',
+          id: '53',
+          title: 'Reference service',
           number: '1.4.1',
           contains: [
             {
               title: 'Condition : can reference service.',
-              id: 'article1',
+              id: '54',
               number: '1.4.1.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '55',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -418,17 +345,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection2',
-          title: 'Create metadata',
+          id: '56',
+          title: 'Create service metadata',
           number: '1.4.2',
           contains: [
             {
               title: 'Condition : can create metadata.',
-              id: 'article1',
+              id: '57',
               number: '1.4.2.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '58',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -439,17 +366,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection3',
-          title: 'Update metadata',
+          id: '59',
+          title: 'Edit service metadata',
           number: '1.4.3',
           contains: [
             {
-              title: 'Condition : can update metadata.',
-              id: 'article1',
+              title: 'Condition : can edit metadata.',
+              id: '60',
               number: '1.4.3.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '61',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -460,17 +387,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection4',
-          title: 'Delete metadata',
+          id: '62',
+          title: 'Remove service metadata',
           number: '1.4.4',
           contains: [
             {
-              title: 'Condition: can delete metadata.',
-              id: 'article1',
+              title: 'Condition: can remove metadata.',
+              id: '63',
               number: '1.4.4.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '64',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -481,17 +408,17 @@ export const mockedGovernanceChapter: Chapter = {
           ]
         },
         {
-          id: 'subsection5',
-          title: 'Dereference dataset',
+          id: '65',
+          title: 'Dereference service',
           number: '1.4.5',
           contains: [
             {
               title: 'Condition: can dereference service.',
-              id: 'article1',
+              id: '66',
               number: '1.4.5.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '67',
                   title: 'Users',
                   description:
                     'did:key:0x04d1f1b8f8a7a28f9a5a254c326a963a22f5a5b5d5f5e5d5c5b5a5958575655',
@@ -504,42 +431,35 @@ export const mockedGovernanceChapter: Chapter = {
       ]
     },
     {
-      id: 'section5',
+      id: '68',
       title: 'Business Model',
       number: '1.5',
       contains: [
         {
-          id: 'subsection1',
-          title: 'Dataset retribution',
+          id: '69',
+          title: 'Datasets retribution',
           number: '1.5.1',
           contains: [
             {
-              title: 'Datasets are retributed if',
-              id: 'article1',
+              title: 'Datasets are not retributed',
+              id: '70',
               number: '1.5.1.1',
-              contains: [
-                {
-                  id: 'paragraph1',
-                  title: 'Retribution',
-                  description: 'Datasets are not retributed.',
-                  number: '1.5.1.1.1'
-                }
-              ]
+              contains: []
             }
           ]
         },
         {
-          id: 'subsection2',
+          id: '72',
           title: 'Service retribution',
           number: '1.5.2',
           contains: [
             {
               title: 'Services are retributed if',
-              id: 'article1',
+              id: '73',
               number: '1.5.2.1',
               contains: [
                 {
-                  id: 'paragraph1',
+                  id: '74',
                   title: 'Pricing',
                   description: 'Must be fixed',
                   number: '1.5.2.1.1'
