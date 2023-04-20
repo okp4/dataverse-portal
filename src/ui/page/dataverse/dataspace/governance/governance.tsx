8cae5c0efedbff41a63c827d616ebe3d92330332
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-04-20 12:32:09 +0200

    refactor: remove useless dataspace state

--- src/ui/page/dataverse/dataspace/governance/governance.tsx
+++ src/ui/page/dataverse/dataspace/governance/governance.tsx
@@ -1,8 +1,7 @@
 import type { FC } from 'react'
-import { useState, useCallback, useEffect } from 'react'
+import { useMemo, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { pipe } from 'fp-ts/lib/function'
-import type { Option } from 'fp-ts/Option'
 import * as O from 'fp-ts/Option'
 import type { DataSpace } from '@/ui/page/dataverse/dataverse'
 import { getResourceDetails } from '@/ui/page/dataverse/dataverse'
@@ -20,15 +19,13 @@ export const Governance: FC = () => {
     subsectionId: subsectionIdParams
   } = useParams<string>()
 
-  const [dataspace, setDataspace] = useState<Option<DataSpace>>(O.none)
-
   const sections = mockedGovernanceChapter.contains
   const currentSection = sections.find(section => section.id === sectionIdParams)
   const currentSubsection = currentSection?.contains.find(
     subsection => subsection.id === subsectionIdParams
   )
 
-  const getNavigationPath = useCallback((): string | null => {
+  const navigationPath = useMemo((): string | undefined => {
     const governanceBasePath = `/dataverse/dataspace/${dataspaceId}/governance`
     const firstSection = sections[0]
     const firstSubsection = firstSection.contains[0]
@@ -44,8 +41,6 @@ export const Governance: FC = () => {
     if (sectionIdParams && !subsectionIdParams && currentSection) {
       return `${governanceBasePath}/${currentSection.id}/${currentSection.contains[0].id}`
     }
-
-    return null
   }, [
     dataspaceId,
     sectionIdParams,
@@ -55,36 +50,38 @@ export const Governance: FC = () => {
     sections
   ])
 
-  useEffect(() => {
-    const navigationPath = getNavigationPath()
+  const dataspaceLabel = useMemo(
+    () =>
+      pipe(
+        O.fromNullable(dataspaceId),
+        O.chain(getResourceDetails),
+        O.filter(isDataSpace),
+        O.map((dataspace: DataSpace) => dataspace.label),
+        O.toNullable
+      ),
+    [dataspaceId]
+  )
 
+  useEffect(() => {
     if (navigationPath) {
       return navigate(navigationPath, { replace: true })
     }
     navigate('/404')
-  }, [navigate, getNavigationPath])
+  }, [navigate, navigationPath])
 
-  useEffect(() => {
-    pipe(
-      O.fromNullable(dataspaceId),
-      O.chain(getResourceDetails),
-      O.filter(isDataSpace),
-      setDataspace
-    )
-  }, [dataspaceId])
+  if (!dataspaceLabel) {
+    return <p>dataspace not found</p>
+  }
 
   if (!dataspaceId) return null
 
-  return O.match(
-    () => <p>dataspace not found</p>,
-    ({ label }: DataSpace) => (
-      <GovernanceContent
-        activeSection={currentSection}
-        activeSubsection={currentSubsection}
-        dataspaceId={dataspaceId}
-        label={label}
-        sections={sections}
-      />
-    )
-  )(dataspace)
+  return (
+    <GovernanceContent
+      activeSection={currentSection}
+      activeSubsection={currentSubsection}
+      dataspaceId={dataspaceId}
+      label={dataspaceLabel}
+      sections={sections}
+    />
+  )
 }
