6a9a260649276acc0f06990a680249d9c1314925
ccamel <camel.christophe@gmail.com>
2023-05-03 22:17:53 +0200

    feat(store): declare useWalletStore hook

--- src/ui/store/index.ts
+++ src/ui/store/index.ts
@@ -5,7 +5,9 @@ import type { DataverseStore } from '@/domain/dataverse/aggregate'
 import type { NotificationAggregate } from '@/domain/notification/aggregate'
 import { notificationAggregate } from '@/domain/notification/aggregate'
 import { sparqlGateway } from '@/infra/dataverse/sparql/sparqlGateway'
-import type { ActionType } from '@/ui/component/toast/toast'
+import type { ActionType} from '@/ui/component/toast/toast';
+import { useStore } from 'zustand'
+import * as Wallet from '@/domain/wallet/domain'
 
 export const dataverseStore = dataverseAggregate(sparqlGateway) as StoreApi<DataverseStore>
 
@@ -14,3 +16,9 @@ export const useNotificationStore = <T>(
   selector: (state: NotificationAggregate<ActionType>) => T,
   equals?: (a: T, b: T) => boolean
 ): T => useStore(notificationStore, selector, equals)
+
+const walletStore = Wallet.storeFactory()
+export const useWalletStore = <T>(
+  selector: (state: Wallet.DomainAPI) => T,
+  equals?: (a: T, b: T) => boolean
+): T => useStore(walletStore, selector, equals)
