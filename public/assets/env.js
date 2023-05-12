8b354af5bbd6bdcecf8dbc093c76cb46a4e56129
lauren olivier <laurenzolivier@gmail.com>
2023-05-12 10:23:10 +0200

    feat(env): add chains & url to env

--- public/assets/env.js
+++ public/assets/env.js
@@ -1,3 +1,42 @@
+const sharedChainConf = {
+  stakeCurrency: {
+    coinDenom: 'KNOW',
+    coinMinimalDenom: 'uknow',
+    coinDecimals: 6
+  },
+  bip44: {
+    coinType: 118
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: 'okp4',
+    bech32PrefixAccPub: 'okp4pub',
+    bech32PrefixValAddr: 'okp4valoper',
+    bech32PrefixValPub: 'okp4valoperpub',
+    bech32PrefixConsAddr: 'okp4valcons',
+    bech32PrefixConsPub: 'okp4valconspub'
+  },
+  currencies: [
+    {
+      coinDenom: 'KNOW',
+      coinMinimalDenom: 'uknow',
+      coinDecimals: 6
+    }
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: 'KNOW',
+      coinMinimalDenom: 'uknow',
+      coinDecimals: 6
+    }
+  ],
+  coinType: 118,
+  gasPriceStep: {
+    low: 0.01,
+    average: 0.025,
+    high: 0.03
+  }
+}
+
 const APP_ENV = {
   urls: {
     'social:twitter': 'https://twitter.com/OKP4_Protocol',
@@ -6,9 +45,33 @@ const APP_ENV = {
     'social:medium': 'https://blog.okp4.network',
     'form:feedback': 'https://okp4.typeform.com/to/TNyFBH72',
     'form:error': 'https://okp4.typeform.com/to/GkWxots6',
-    'about:okp4': 'https://okp4.network'
+    'about:okp4': 'https://okp4.network',
+    'extension:keplr': 'https://www.keplr.app/download'
   },
   sparql: {
     endpoint: 'https://api.staging.okp4.space/ontology/world/sparql'
-  }
+  },
+  chains: [
+    {
+      id: 'okp4-nemeton-1',
+      chainName: 'OKP4 nemeton',
+      rpc: 'https://api.testnet.okp4.network:443/rpc',
+      rest: 'https://api.testnet.okp4.network/',
+      sharedChainConf
+    },
+    {
+      id: 'okp4-devnet-1',
+      chainName: 'OKP4 devnet',
+      rpc: 'https://api.devnet.okp4.network:443/rpc',
+      rest: 'https://api.devnet.okp4.network/',
+      sharedChainConf
+    },
+    {
+      id: 'okp4-devnet-staging-1',
+      chainName: 'OKP4 devnet staging',
+      rpc: 'https://api.devnet.staging.okp4.network:443/rpc',
+      rest: 'https://api.devnet.staging.okp4.network:443/rpc',
+      sharedChainConf
+    }
+  ]
 }
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,26 @@
+type ChainInfo = {
+  id: ChainId
+  rpc: string
+  rest: string
+  chainName: string
+  stakeCurrency: Currency
+  walletUrl?: string
+  walletUrlForStaking?: string
+  bip44: BIP44
+  alternativeBIP44s?: BIP44[]
+  bech32Config: Bech32Config
+  currencies: AppCurrency[]
+  feeCurrencies: Currency[]
+  coinType?: number
+  gasPriceStep?: {
+    low: number
+    average: number
+    high: number
+  }
+  features?: string[]
+  beta?: boolean
+}
+
 declare const APP_ENV: {
   urls: Record<
     | 'social:twitter'
@@ -6,8 +29,10 @@ declare const APP_ENV: {
     | 'social:medium'
     | 'form:feedback'
     | 'form:error'
-    | 'about:okp4',
+    | 'about:okp4'
+    | 'extension:keplr',
     string
   >
   sparql: Record<'endpoint', string>
+  chains: ChainInfo[]
 }
