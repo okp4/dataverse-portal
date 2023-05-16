98d3a8d36f921b27c3579e356b6bf0d2350da6ad
ErikssonJoakim <j.eriksson@okp4.com>
2023-05-16 18:55:50 +0200

    feat(copytoclipboard): add copy state with icons

--- src/ui/component/copyToClipboard/copyToClipboard.tsx
+++ src/ui/component/copyToClipboard/copyToClipboard.tsx
@@ -1,7 +1,10 @@
+import { useCallback, useState } from 'react'
 import { useAppStore } from '@/ui/store/appStore'
-import { useCallback } from 'react'
 import { Icon } from '@/ui/component/icon/icon'
+import classNames from 'classnames'
 import './copyToClipboard.scss'
+
+type CopyState = 'copy' | 'success' | 'error'
 
 type CopyToClipBoardProps = {
   textToCopy: string
@@ -10,14 +13,17 @@ type CopyToClipBoardProps = {
 
 export const CopyToClipboard = ({ onCopied, textToCopy }: CopyToClipBoardProps): JSX.Element => {
   const theme = useAppStore(state => state.theme)
+  const [copyState, setCopyState] = useState<CopyState>('copy')
 
   const handleClipboardCopy = useCallback(async (): Promise<void> => {
     navigator.clipboard
       .writeText(textToCopy)
       .then(() => {
+        setCopyState('success')
         onCopied?.(true)
       })
       .catch((error: Error) => {
+        setCopyState('error')
         console.error(error.message)
         onCopied?.(false)
       })
@@ -25,8 +31,19 @@ export const CopyToClipboard = ({ onCopied, textToCopy }: CopyToClipBoardProps):
 
   return (
     <div className="okp4-dataverse-portal-copy-paste-main">
-      <div className="okp4-dataverse-portal-copy-paste-icon" onClick={handleClipboardCopy}>
-        <Icon name={`copy-${theme}`} />
+      <div
+        className={classNames('okp4-dataverse-portal-copy-paste-icons-container', copyState)}
+        onClick={handleClipboardCopy}
+      >
+        <div className="okp4-dataverse-portal-copy-paste-icon copy">
+          <Icon name={`copy-${theme}`} />
+        </div>
+        <div className="okp4-dataverse-portal-copy-paste-icon success">
+          <Icon name="check" />
+        </div>
+        <div className="okp4-dataverse-portal-copy-paste-icon error">
+          <Icon name="error" />
+        </div>
       </div>
     </div>
   )
