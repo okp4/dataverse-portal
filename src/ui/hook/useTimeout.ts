844d37f65985117a85f79816329d442acb847626
ErikssonJoakim <j.eriksson@okp4.com>
2023-05-19 12:07:30 +0200

    feat(hooks): introduce usecopytoclipboard and usetimeout

---
+++ src/ui/hook/useTimeout.ts
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+type UseTimeout = () => void
+
+export const useTimeout = (callback: () => void, delay: number | null): UseTimeout => {
+  const timeoutRef = useRef<number | null>(null)
+
+  const set = useCallback(() => {
+    timeoutRef.current = delay !== null ? setTimeout(() => callback(), delay) : null
+  }, [callback, delay])
+
+  const disposerFn = useCallback(() => {
+    timeoutRef.current && clearTimeout(timeoutRef.current)
+  }, [])
+
+  useEffect(() => {
+    set()
+    return disposerFn
+  }, [set, disposerFn])
+
+  return disposerFn
+}
