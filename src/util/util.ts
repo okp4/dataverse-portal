a71d6684401eb65a5200c3a7878b39cfd4c744a7
lauren olivier <laurenzolivier@gmail.com>
2023-05-22 15:55:04 +0200

    feat(utils): add taskLogger function

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,33 @@
 import type * as O from 'fp-ts/Option'
 import * as A from 'fp-ts/Array'
-import { pipe } from 'fp-ts/function'
+import * as T from 'fp-ts/Task'
+import { flow, pipe } from 'fp-ts/function'
+import { error, info, log, warn } from 'fp-ts/lib/Console'
+import type { IO } from 'fp-ts/lib/IO'
+import type { Task } from 'fp-ts/lib/Task'
 
 export const getURILastElement = (str: string): O.Option<string> => pipe(str.split('/'), A.last)
+
+export const taskLogger =
+  <T>(message?: string, logLevel: 'error' | 'info' | 'log' | 'warn' = 'log') =>
+  (task: Task<T>): Task<T> =>
+    pipe(
+      task,
+      T.chainFirstIOK(
+        flow(
+          value => (message ? { message, value } : { value }),
+          ((): (<A>(a: A) => IO<void>) => {
+            switch (logLevel) {
+              case 'error':
+                return error
+              case 'info':
+                return info
+              case 'log':
+                return log
+              case 'warn':
+                return warn
+            }
+          })()
+        )
+      )
+    )
