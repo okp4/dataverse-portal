e66836d5ae02c5967c23621468608aacb8d707a0
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-05-26 18:24:53 +0200

    feat: create text highlighter component

---
+++ src/ui/component/textHighlighter/textHighlighter.tsx
@@ -0,0 +1,27 @@
+import type { FC } from 'react'
+import { splitTextByTerm } from '@/util/splitTextByTerm/splitTextByTerm'
+
+type TextHighlighterProps = {
+  text: string
+  termToHighlight: string
+}
+export const TextHighlighter: FC<TextHighlighterProps> = ({
+  text,
+  termToHighlight: textToHighlight
+}) => {
+  const parts = splitTextByTerm(text, textToHighlight)
+
+  return (
+    <>
+      {parts.map((part, index) =>
+        part.toLowerCase() === textToHighlight.toLowerCase() ? (
+          <span className="text-highlight" key={index}>
+            {part}
+          </span>
+        ) : (
+          <span key={index}>{part}</span>
+        )
+      )}
+    </>
+  )
+}
