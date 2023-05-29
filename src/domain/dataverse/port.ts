68e0a9c0af0ee0f747468f34a81770f19399e82e
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-05-29 18:02:49 +0200

    refactor(domain): improve type dfinitions in dataverse port

--- src/domain/dataverse/port.ts
+++ src/domain/dataverse/port.ts
@@ -1,15 +1,14 @@
 import type * as TE from 'fp-ts/TaskEither'
-import type { DataverseEntity } from '@/domain/dataverse/entity'
-import type { ByTypeQueryFilter, DataverseElementType } from './aggregate'
+import type { DataverseElement } from '@/domain/dataverse/entity'
 
-export type RetrieveDataverseByTypeQueryFilter = ByTypeQueryFilter
-export type DataverseElementTypeFilter = DataverseElementType
+export type DataverseElementType = 'DataSpace' | 'Dataset' | 'Service'
+export type ByTypeQueryFilter = DataverseElementType[] | 'all'
 
 export type RetrieveDataverseQueryFilters = {
-  byType: RetrieveDataverseByTypeQueryFilter
+  byType: ByTypeQueryFilter
 }
 
-export type RetrieveDataverseResult = { data: DataverseEntity; query: { hasNext: boolean } }
+export type RetrieveDataverseResult = { data: DataverseElement[]; query: { hasNext: boolean } }
 
 export type DataversePort = {
   retrieveDataverse: (
