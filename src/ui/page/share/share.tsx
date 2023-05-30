0c288f884b09e0258ee627feb28eb39ebf500526
Anja Miscevic <anja.miscevic@myy.haaga-helia.fi>
2023-05-30 09:25:50 +0200

    refactor: code improvements

--- src/ui/page/share/share.tsx
+++ src/ui/page/share/share.tsx
@@ -1,7 +1,6 @@
 import type { FC } from 'react'
 import { Card } from '@/ui/component/card/card'
 import { useTranslation } from 'react-i18next'
-import classNames from 'classnames'
 import './share.scss'
 import './i18n/index'
 
@@ -10,25 +9,23 @@ export const Share: FC = () => {
 
   return (
     <div className="okp4-dataverse-portal-share-page-main">
-      <Card mainClassName={classNames('okp4-dataverse-portal-share-page-card', 'left')}>
+      <Card mainClassName="okp4-dataverse-portal-share-page-card left">
         <>
-          <div className="okp4-dataverse-portal-share-page-ilustration"> </div>
+          <div className="okp4-dataverse-portal-share-page-ilustration" />
           <div className="okp4-dataverse-portal-share-page-text-container">
             <h3>{t('share.dataset.title')}</h3>
             <p>{t('share.dataset.description')}</p>
           </div>
         </>
       </Card>
-      <Card
-        mainClassName={classNames('okp4-dataverse-portal-share-page-card', 'right', 'disabled')}
-      >
+      <Card mainClassName="okp4-dataverse-portal-share-page-card right">
         <>
-          <div className="okp4-dataverse-portal-share-page-ilustration"> </div>
+          <div className="okp4-dataverse-portal-share-page-ilustration" />
           <div className="okp4-dataverse-portal-share-page-text-container">
             <h3>{t('share.service.title')}</h3>
             <p>{t('share.service.description')}</p>
             <h3 className="okp4-dataverse-portal-share-page-text-disabled">
-              {t('share.disabled')}
+              {t('share.soonAvailable')}
             </h3>
           </div>
         </>
