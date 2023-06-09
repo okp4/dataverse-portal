6b7361b575e639a32d94ab6ff06964767b308d24
lauren olivier <laurenzolivier@gmail.com>
2023-06-09 09:18:26 +0200

    feat(serviceStorageSelection): add infinite scroll

--- src/ui/page/share/dataset/steps/serviceStorageSelection/serviceStorageSelection.tsx
+++ src/ui/page/share/dataset/steps/serviceStorageSelection/serviceStorageSelection.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect } from 'react'
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { SearchBar } from '@/ui/component/searchbar/searchbar'
 import { useDataverseStore } from '@/ui/store'
 import { DataverseItemCard } from '@/ui/view/dataverse/component/dataverseItemCard/dataverseItemCard'
@@ -8,6 +9,8 @@ import { Button } from '@/ui/component/button/button'
 import { loadingDataverseCards } from '@/ui/view/loadingDataverseCards/loadingDataverseCards'
 import type { DataverseItem } from '@/ui/types'
 import { activeLanguageWithDefault } from '@/ui/languages/languages'
+import { LottieLoader } from '@/ui/component/loader/lottieLoader'
+import threeDots from '@/ui/asset/loader/threeDots.json'
 import '../../../i18n/index'
 import './serviceStorageSelection.scss'
 
@@ -16,16 +19,16 @@ export const ServiceStorageSelection: FC = () => {
   const { t } = useTranslation(['common', 'share', 'publisher'])
   const currentLng = activeLanguageWithDefault().lng
 
-  const { loadDataverse, dataverse, setByTypeFilter, isLoading, setLanguage } = useDataverseStore(
-    state => ({
+  const { loadDataverse, dataverse, setByTypeFilter, isLoading, hasNext, setLanguage } =
+    useDataverseStore(state => ({
       loadDataverse: state.loadDataverse,
       dataverse: state.dataverse,
       isLoading: state.isLoading,
       hasNext: state.hasNext,
       setLanguage: state.setLanguage,
       setByTypeFilter: state.setByTypeFilter
-    })
-  )
+    }))
+
   const handleServiceSearch = useCallback((searchTearm: string) => {
     console.log(searchTearm)
   }, [])
@@ -56,28 +59,40 @@ export const ServiceStorageSelection: FC = () => {
         placeholder={t('share:share.dataset.serviceStorageSearch')}
         value=""
       />
-      <div className="okp4-dataverse-portal-share-dataset-page-services-container">
-        <div className="okp4-dataverse-portal-share-dataset-page-services">
-          {!dataverse()().length && isLoading()()
-            ? loadingDataverseCards(12)
-            : dataverse()().map(({ id, properties }) => {
-                const publisher = properties.find(p => p.property === 'publisher')?.value ?? ''
-                const publisherDescription = `${t('metadata:publisher')} **${publisher}**`
-                return (
-                  <DataverseItemCard
-                    button={<Button label={t('select')} onClick={handleSelect(id)} />}
-                    key={id}
-                    label={properties.find(p => p.property === 'title')?.value ?? ''}
-                    topic={publisherDescription}
-                    type={
-                      properties
-                        .find(p => p.property === 'type')
-                        ?.value.toLowerCase() as DataverseItem
-                    }
-                  />
-                )
-              })}
-        </div>
+      <div
+        className="okp4-dataverse-portal-share-dataset-page-services-container"
+        id="scrollable-container"
+      >
+        <InfiniteScroll
+          dataLength={dataverse()().length}
+          hasMore={hasNext()()}
+          loader={dataverse()().length && <LottieLoader animationData={threeDots} />}
+          next={loadDataverse()}
+          scrollThreshold={0.91}
+          scrollableTarget="scrollable-container"
+        >
+          <div className="okp4-dataverse-portal-share-dataset-page-services">
+            {!dataverse()().length && isLoading()()
+              ? loadingDataverseCards(12)
+              : dataverse()().map(({ id, properties }) => {
+                  const publisher = properties.find(p => p.property === 'publisher')?.value ?? ''
+                  const publisherDescription = `${t('metadata:publisher')} **${publisher}**`
+                  return (
+                    <DataverseItemCard
+                      button={<Button label={t('select')} onClick={handleSelect(id)} />}
+                      key={id}
+                      label={properties.find(p => p.property === 'title')?.value ?? ''}
+                      topic={publisherDescription}
+                      type={
+                        properties
+                          .find(p => p.property === 'type')
+                          ?.value.toLowerCase() as DataverseItem
+                      }
+                    />
+                  )
+                })}
+          </div>
+        </InfiniteScroll>
       </div>
     </div>
   )
