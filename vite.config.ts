519a87a51434158dcd0e7dd2c418682396c226f6
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-06-12 12:09:22 +0200

    build(scss): integrate autoprefixer and remove manual vendor prefixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
 import htmlConfig from 'vite-plugin-html-config'
+import autoprefixer from 'autoprefixer'
 
 import htmlConfigOptions from './config/html-config'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [htmlConfig(htmlConfigOptions), react(), svgr()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})]
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
