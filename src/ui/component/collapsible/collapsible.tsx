c61892a1dc51154a157c604613eb04c56a8cb5ea
ErikssonJoakim <j.eriksson@okp4.com>
2023-06-20 17:33:32 +0200

    feat(collapsible): make animation animate width change

--- src/ui/component/collapsible/collapsible.tsx
+++ src/ui/component/collapsible/collapsible.tsx
@@ -1,5 +1,5 @@
+import { useEffect, useRef, useState } from 'react'
 import type { FC } from 'react'
-import { useState } from 'react'
 import classNames from 'classnames'
 import * as RCollapsible from '@radix-ui/react-collapsible'
 import type { IconName } from '@/ui/component/icon/icon'
@@ -25,12 +25,19 @@ export const Collapsible: FC<CollapsibleProps> = ({
   triggerClassName,
   iconName = 'chevron'
 }) => {
+  const [triggerInitWidth, setTriggerInitWidth] = useState<number | null>(null)
   const [isOpen, setIsOpen] = useState(open)
+  const triggerRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    setTriggerInitWidth(triggerRef.current?.clientWidth ?? null)
+  }, [])
 
   return (
     <RCollapsible.Root className={rootClassName} onOpenChange={setIsOpen} open={isOpen}>
       <RCollapsible.Trigger
         className={classNames('okp4-dataverse-portal-collapsible-trigger-button', triggerClassName)}
+        ref={triggerRef}
       >
         {trigger}
         <div
@@ -43,6 +50,12 @@ export const Collapsible: FC<CollapsibleProps> = ({
       </RCollapsible.Trigger>
       <RCollapsible.Content
         className={classNames('okp4-dataverse-portal-collapsible-content', contentClassName)}
+        style={{
+          // Copying the behaviour of the Radix-ui collapsible, for the slide animations
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          ['--okp4-collapsible-trigger-initial-width' as any]:
+            triggerInitWidth && `${triggerInitWidth}px`
+        }}
       >
         {content}
       </RCollapsible.Content>
