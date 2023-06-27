23d3031487e52c7212b5a54432c36a68433db476
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-06-27 16:08:44 +0200

    feat: make list-item content required; handle columns positioning

--- src/ui/component/list/list.tsx
+++ src/ui/component/list/list.tsx
@@ -1,13 +1,23 @@
-import type { FC, ReactNode } from 'react'
+import type { FC, ReactElement, ReactNode } from 'react'
 import classNames from 'classnames'
 import './list.scss'
 
 type ListItemProps = {
-  leftElement?: ReactNode
-  content?: ReactNode
-  rightElement?: ReactNode
+  content: ReactElement
+  leftElement?: ReactElement
+  rightElement?: ReactElement
   onClick?: (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
   className?: string
+}
+
+const gridTemplateColumnClasses = (
+  leftElement?: ReactElement,
+  rightElement?: ReactElement
+): string => {
+  if (leftElement && rightElement) return 'list-item-3-columns'
+  if (leftElement) return 'list-item-2-columns-left'
+  if (rightElement) return 'list-item-2-columns-right'
+  return 'list-item-1-column'
 }
 
 export const ListItem: FC<ListItemProps> = ({
@@ -18,9 +28,12 @@ export const ListItem: FC<ListItemProps> = ({
   className
 }) => (
   <li
-    className={classNames('okp4-dataverse-portal-list-item-main', className, {
-      clickable: onClick
-    })}
+    className={classNames(
+      'okp4-dataverse-portal-list-item-main',
+      { clickable: onClick },
+      gridTemplateColumnClasses(leftElement, rightElement),
+      className
+    )}
     onClick={onClick}
   >
     {leftElement}
