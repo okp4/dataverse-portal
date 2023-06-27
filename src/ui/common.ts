87f3f09795f0424a5a793b603b486ddb28e6ef1a
lauren olivier <laurenzolivier@gmail.com>
2023-06-27 11:17:22 +0200

    refactor(types): update dataverse item dataspace to zone

--- src/ui/common.ts
+++ src/ui/common.ts
@@ -4,7 +4,7 @@ export const renderItemTypeColor = (type: DataverseItem): ColorVariant => {
   switch (type) {
     case 'service':
       return 'primary-color'
-    case 'dataspace':
+    case 'zone':
       return 'primary-color-variant-3'
     case 'dataset':
       return 'primary-color-variant-4'
--- src/ui/types.ts
+++ src/ui/types.ts
@@ -1,2 +1,2 @@
-export type DataverseItem = 'dataspace' | 'dataset' | 'service'
+export type DataverseItem = 'zone' | 'dataset' | 'service'
 export type ColorVariant = 'primary-color' | 'primary-color-variant-3' | 'primary-color-variant-4'
