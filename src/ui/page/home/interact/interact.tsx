570edf90026d43431632421b2b9e8951ae22ccfb
Anja <a.miscevic@okp4.com>
2023-06-27 10:15:47 +0200

    feat(homepage): enable share action card

--- src/ui/page/home/interact/interact.tsx
+++ src/ui/page/home/interact/interact.tsx
@@ -1,9 +1,11 @@
 import type { FC } from 'react'
-import { useMemo } from 'react'
+import { useMemo, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import type { CardBackground } from '@/ui/component/card/card'
+import { useNavigate } from 'react-router-dom'
 import classNames from 'classnames'
+import type { CardBackground } from '@/ui/component/card/card'
 import { Card } from '@/ui/component/card/card'
+import { routes } from '@/ui/routes'
 import './interact.scss'
 import '../i18n/index'
 
@@ -14,21 +16,25 @@ type InteractCard = {
   style: {
     backgroundColor: CardBackground
   }
+  path?: string
 }
 
 // eslint-disable-next-line max-lines-per-function
 export const Interact: FC = () => {
   const { t } = useTranslation('home')
 
+  const navigate = useNavigate()
+  const handleNavigateTo = useCallback((path: string) => () => navigate(path), [navigate])
+
   const interactCards: InteractCard[] = useMemo(
     () => [
       {
         action: t('home.blocks.interact.data-services.action'),
         category: t('home.blocks.interact.data-services.category'),
-        disabled: true,
         style: {
           backgroundColor: 'primary-color-variant-1'
-        }
+        },
+        path: routes.share
       },
       {
         action: t('home.blocks.interact.knowledge.action'),
@@ -61,11 +67,12 @@ export const Interact: FC = () => {
   return (
     <>
       <h1>{t('home.blocks.interact.label')}</h1>
-      {interactCards.map(({ action, category, disabled, style }, index) => (
+      {interactCards.map(({ action, category, disabled, style, path }, index) => (
         <Card
           background={style.backgroundColor}
           key={index}
           mainClassName={classNames('okp4-dataverse-portal-home-page-interact-card', { disabled })}
+          {...(path && { onClick: handleNavigateTo(path) })}
         >
           <div>
             <h3>{action}</h3>
