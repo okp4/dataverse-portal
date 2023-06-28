105169bc609c496d66393e2f0b794d7fad4cdd64
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-06-28 18:15:27 +0200

    feat(domain): add file query

---
+++ src/domain/file/query.ts
@@ -0,0 +1,18 @@
+import type { IO } from 'fp-ts/lib/IO'
+
+export type FileId = string
+export type MediaType = string
+
+export type FileDescriptor = {
+  id: FileId
+  name: string
+  size: number
+  type: MediaType
+}
+
+export type FilesDescriptor = FileDescriptor[]
+
+export type Query = {
+  // Get the base properties of in memory files.
+  filesDescriptor: () => IO<FilesDescriptor>
+}
