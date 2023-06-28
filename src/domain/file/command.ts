7be3d9776fe68dc2f575ad891f536c02e4f6db04
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-06-28 18:14:38 +0200

    feat(domain): add file commands

---
+++ src/domain/file/command.ts
@@ -0,0 +1,45 @@
+import type { IOEither } from 'fp-ts/IOEither'
+
+export type MediaType = string
+
+export type FileId = string
+
+export type StoreFileInput = {
+  id: FileId
+  name: string
+  path: string
+  size: number
+  type: MediaType
+  stream: ReadableStream
+}
+
+export type StoreFilesInput = StoreFileInput[]
+
+export const ResourceConflictError = () =>
+  ({
+    _tag: 'resource-conflict'
+  } as const)
+
+/**
+ *  Error thrown when an id of a file to be stored already exists in memory,
+ * or if multiple files in the StoreFileInput paylaod have the same id
+ */
+export type ResourceConflictError = ReturnType<typeof ResourceConflictError>
+
+export const ResourceNotFoudError = (fileId: FileId) =>
+  ({
+    _tag: 'resource-not-found',
+    fileId
+  } as const)
+
+/**
+ *  Error for when the the id of a file to be removed is not found in the list of stored files in memory.
+ */
+export type ResourceNotFoudError = ReturnType<typeof ResourceNotFoudError>
+
+export type Command = {
+  // Store one or multiple files in memory
+  storeFiles: (files: StoreFilesInput) => IOEither<ResourceConflictError, void>
+  // Remove a file in memory by a given file id
+  removeFile: (fileId: FileId) => IOEither<ResourceNotFoudError, void>
+}
