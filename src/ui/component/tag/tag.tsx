76d4b3489a268b40f97f9ba40d015f44284c5350
Oussama <oussama.aitbaziz@gmail.com>
2023-07-11 09:55:03 +0200

    feat: handle tag delete action

--- src/ui/component/tag/tag.tsx
+++ src/ui/component/tag/tag.tsx
@@ -1,12 +1,34 @@
-import type { FC } from 'react'
+import { useCallback, type FC } from 'react'
+import { Icon } from '../icon/icon'
+
 import './tag.scss'
 
 type TagProps = {
   tagName: string
+  onDelete?: () => void
 }
 
-export const Tag: FC<TagProps> = ({ tagName }) => (
-  <div className="okp4-dataverse-portal-tag-main">
-    <div className="okp4-dataverse-portal-tag-name">{tagName}</div>
-  </div>
-)
+export const Tag: FC<TagProps> = ({ tagName, onDelete }) => {
+  const handleClick = useCallback(() => {
+    if (onDelete) {
+      onDelete()
+    }
+  }, [onDelete])
+
+  return (
+    <div className="okp4-dataverse-portal-tag-main">
+      <div className="okp4-dataverse-portal-tag-name">{tagName}</div>
+      {onDelete && (
+        <div
+          className="okp4-dataverse-portal-tag-icon"
+          onClick={handleClick}
+          role="button"
+          tabIndex={0}
+          title="close"
+        >
+          <Icon name="close-dark" />
+        </div>
+      )}
+    </div>
+  )
+}
