54c621501e93353e137d54a2f105fc0bd7137920
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-07-12 17:05:21 +0200

    feat(ui): make setFormItemValue method return either an error or void

--- src/ui/store/slice/shareDataForm.slice.ts
+++ src/ui/store/slice/shareDataForm.slice.ts
@@ -1,12 +1,17 @@
+/* eslint-disable max-lines-per-function */
 import type { StateCreator } from 'zustand'
 import * as A from 'fp-ts/Array'
-import type { IO } from 'fp-ts/IO'
+import type { IOEither } from 'fp-ts/IOEither'
+import * as IOE from 'fp-ts/IOEither'
 import * as IOO from 'fp-ts/IOOption'
+import type { IOOption } from 'fp-ts/IOOption'
 import { flow, pipe } from 'fp-ts/lib/function'
 import type { Eq } from 'fp-ts/lib/Eq'
 import * as S from 'fp-ts/string'
 import * as B from 'fp-ts/boolean'
+import * as E from 'fp-ts/Either'
 import { contramap as eqContramap } from 'fp-ts/Eq'
+import { ResourceNotFoundError } from '@/shared/error'
 
 export type FormItemId = string
 
@@ -26,13 +31,17 @@ export type FormItem = Readonly<{
   required?: boolean
 }>
 
+export type SetFormItemValuePayload = { id: FormItemId; value: FormItemValue }
+
 export type Form = FormItem[]
+
+export type FormPayload = Form
 
 export type ShareDataFormSlice = {
   form: Form
-  init: (initialForm: Form) => IO<void>
-  setFormItemValue: (id: FormItemId, value: FormItemValue) => IO<void>
-  formItemById: (id: FormItemId) => IOO.IOOption<FormItem>
+  init: (payload: FormPayload) => IOEither<void, Error>
+  setFormItemValue: (payload: SetFormItemValuePayload) => IOEither<ResourceNotFoundError, void>
+  formItemById: (id: FormItemId) => IOOption<FormItem>
 }
 
 const eqFormItemId: Eq<FormItemId> = S.Eq
@@ -40,6 +49,19 @@ const eqFormItem: Eq<{ id: string }> = pipe(
   eqFormItemId,
   eqContramap(it => it.id)
 )
+
+const resourceNotFoundError = (resourceId: FormItemId): ResourceNotFoundError =>
+  ResourceNotFoundError(resourceId)
+
+const formIdExists =
+  (state: Form) =>
+  (formItemId: FormItemId): boolean =>
+    A.some((formItem: FormItem) => formItem.id === formItemId)(state)
+
+const setFormItemValueInvariant =
+  (formItemId: FormItemId) =>
+  (state: Form): E.Either<ResourceNotFoundError, FormItemId> =>
+    pipe(formItemId, E.fromPredicate(flow(formIdExists(state)), resourceNotFoundError))
 
 export const createShareDataFormSlice: StateCreator<
   ShareDataFormSlice,
@@ -58,19 +80,39 @@ export const createShareDataFormSlice: StateCreator<
       IOO.fromIO(() => get().form),
       IOO.chainOptionK(flow(A.findFirst(formItem => eqFormItem.equals(formItem, { id }))))
     ),
-  setFormItemValue: (id: FormItemId, value: FormItemValue) => () =>
-    set(state => ({
-      form: pipe(
-        state.form,
-        A.map(formItem =>
+  setFormItemValue: ({ id, value }: SetFormItemValuePayload) =>
+    pipe(
+      id,
+      S.isEmpty,
+      B.match(
+        () =>
           pipe(
-            eqFormItem.equals(formItem, { id }),
-            B.match(
-              () => formItem,
-              () => ({ ...formItem, value })
+            IOE.fromIO(() => get().form),
+            IOE.flatMap(
+              flow(
+                setFormItemValueInvariant(id),
+                IOE.fromEither,
+                IOE.chainIOK(
+                  formItemId => () =>
+                    set(state => ({
+                      form: pipe(
+                        state.form,
+                        A.map(formItem =>
+                          pipe(
+                            eqFormItem.equals(formItem, { id: formItemId }),
+                            B.match(
+                              () => formItem,
+                              () => ({ ...formItem, value })
+                            )
+                          )
+                        )
+                      )
+                    }))
+                )
+              )
             )
-          )
-        )
+          ),
+        () => IOE.of(undefined)
       )
-    }))
+    )
 })
