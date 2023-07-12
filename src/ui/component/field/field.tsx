3735d8642319d06af8928379e01611ab0f0441e0
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-07-12 16:35:58 +0200

    feat: make field component more generic and handle number type

--- src/ui/component/field/field.tsx
+++ src/ui/component/field/field.tsx
@@ -1,13 +1,14 @@
 import { type FC, type ChangeEvent, useCallback, useState, useRef } from 'react'
 import classNames from 'classnames'
 import { Icon } from '@/ui/component/icon/icon'
-import './textField.scss'
+import './field.scss'
 
-type TextFieldProps = {
+type FieldProps = {
   id: string
   label: string
   onChange: (value: string) => void
   value?: string
+  type?: 'text' | 'number'
   error?: string
   required?: boolean
   disabled?: boolean
@@ -18,11 +19,12 @@ type TextFieldProps = {
 }
 
 // eslint-disable-next-line max-lines-per-function
-export const TextField: FC<TextFieldProps> = ({
+export const Field: FC<FieldProps> = ({
   id,
   label,
   onChange,
   value,
+  type = 'text',
   error,
   required = false,
   disabled = false,
@@ -54,7 +56,7 @@ export const TextField: FC<TextFieldProps> = ({
   )
 
   const textFieldInputProps = {
-    className: classNames(textFieldClassNames, 'okp4-dataverse-portal-text-field-input'),
+    className: classNames(textFieldClassNames, 'okp4-dataverse-portal-field-input'),
     disabled,
     id,
     name: id,
@@ -67,13 +69,10 @@ export const TextField: FC<TextFieldProps> = ({
   }
 
   return (
-    <div className={classNames(textFieldClassNames, 'okp4-dataverse-portal-text-field-main')}>
+    <div className={classNames(textFieldClassNames, 'okp4-dataverse-portal-field-main')}>
       {leftElement && (
         <div
-          className={classNames(
-            textFieldClassNames,
-            'okp4-dataverse-portal-text-field-left-element'
-          )}
+          className={classNames(textFieldClassNames, 'okp4-dataverse-portal-field-left-element')}
         >
           {leftElement}
         </div>
@@ -82,29 +81,26 @@ export const TextField: FC<TextFieldProps> = ({
       {multiline ? (
         <textarea {...textFieldInputProps} />
       ) : (
-        <input {...textFieldInputProps} type="text" />
+        <input {...textFieldInputProps} type={type} />
       )}
 
       {rightElement && (
         <div
-          className={classNames(
-            textFieldClassNames,
-            'okp4-dataverse-portal-text-field-right-element'
-          )}
+          className={classNames(textFieldClassNames, 'okp4-dataverse-portal-field-right-element')}
         >
           {rightElement}
         </div>
       )}
       <label
-        className={classNames(textFieldClassNames, 'okp4-dataverse-portal-text-field-label')}
+        className={classNames(textFieldClassNames, 'okp4-dataverse-portal-field-label')}
         htmlFor={id}
       >
         {required ? label + '*' : label}
       </label>
       {error && (
-        <p className="okp4-dataverse-portal-text-field-error">
+        <p className="okp4-dataverse-portal-field-error">
           <Icon name="info-light" />
-          <span className="okp4-dataverse-portal-text-field-error-message">{error}</span>
+          <span className="okp4-dataverse-portal-field-error-message">{error}</span>
         </p>
       )}
     </div>
