e2dc6e53b0e249793d400a5e66b2341b497a1184
lauren olivier <laurenzolivier@gmail.com>
2023-07-13 16:34:44 +0200

    feat(share): add step 3 to stepper

--- src/ui/page/share/dataset/shareDataset.tsx
+++ src/ui/page/share/dataset/shareDataset.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ServiceStorageSelection } from './steps/serviceStorageSelection/serviceStorageSelection'
 import { DataSelection } from './steps/dataSelection/dataSelection'
+import { MetadataFilling } from './steps/metadataFilling/metadataFilling'
 import { Stepper } from '@/ui/component/stepper/stepper'
 import type { StepElement } from '@/ui/component/stepper/stepper'
 import '../i18n/index'
@@ -12,8 +13,9 @@ export const ShareDataset: FC = () => {
 
   const storageServiceSelection: StepElement = { id: 'step1', content: <ServiceStorageSelection /> }
   const dataSelection: StepElement = { id: 'step2', content: <DataSelection /> }
+  const metadataFilling: StepElement = { id: 'step3', content: <MetadataFilling /> }
 
-  const steps = [storageServiceSelection, dataSelection]
+  const steps = [storageServiceSelection, dataSelection, metadataFilling]
 
   return (
     <div className="okp4-dataverse-portal-share-dataset-page-main">
