375fc96326f5ea7e75303600f92970cc54ecc490
Anja Miscevic <anja.miscevic@myy.haaga-helia.fi>
2023-07-20 14:23:37 +0200

    feat: add metadata

--- src/ui/page/share/dataset/steps/summary/summary.tsx
+++ src/ui/page/share/dataset/steps/summary/summary.tsx
@@ -31,34 +31,20 @@ export const Summary: FC = () => {
     )
   }))
 
+  const formItems = useAppStore(state => state.shareData.form)
+  const filteredformItems = formItems.filter(item => item.label !== 'fee')
+
   return (
     <div className="okp4-dataverse-portal-share-dataset-summary-container">
       <h2>{t('share:share.dataset.summary')}</h2>
       <div className="okp4-dataverse-portal-share-dataset-summary-left">
-        <div className="okp4-dataverse-portal-share-dataset-summary-text">
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-            amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-            labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-            consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-            nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
-            quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-            voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
-            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
-            dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
-            incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-            veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
-            ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate
-            velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-            voluptas nulla pariatur?
-          </p>
+        <div className="okp4-dataverse-portal-share-dataset-summary-text-container">
+          {filteredformItems.map(({ label, value }, index) => (
+            <div className="okp4-dataverse-portal-share-dataset-summary-text" key={index}>
+              <h3>{label.replace(/([A-Z])/g, ' $1')}</h3>
+              <span>{value.toString()}</span>
+            </div>
+          ))}
         </div>
       </div>
       <div className="okp4-dataverse-portal-share-dataset-summary-right">
