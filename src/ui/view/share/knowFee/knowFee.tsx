60a0df88c12918c6f527490e8d15847639665b86
lauren olivier <laurenzolivier@gmail.com>
2023-07-24 11:20:05 +0200

    refactor(knowFee): add label prop

--- src/ui/view/share/knowFee/knowFee.tsx
+++ src/ui/view/share/knowFee/knowFee.tsx
@@ -3,22 +3,18 @@ import classNames from 'classnames'
 import { useAppStore } from '@/ui/store'
 import type { Field } from '@/ui/component/field/field'
 import './knowFee.scss'
-import { useTranslation } from 'react-i18next'
 
 type KnowFeeProps = {
+  label: string
   field: ReactElement<typeof Field>
 }
 
-export const KnowFee: FC<KnowFeeProps> = ({ field }) => {
-  const { t } = useTranslation('share')
-
+export const KnowFee: FC<KnowFeeProps> = ({ field, label }) => {
   const theme = useAppStore(store => store.theme)
 
   return (
     <div className={classNames('okp4-dataverse-portal-share-data-know-fee-main', theme)}>
-      <label className="okp4-dataverse-portal-share-data-know-fee-label">
-        {t('share:share.dataset.knowFee')}
-      </label>
+      <label className="okp4-dataverse-portal-share-data-know-fee-label">{label}</label>
       <div className="okp4-dataverse-portal-share-data-know-fee-field">{field}</div>
     </div>
   )
