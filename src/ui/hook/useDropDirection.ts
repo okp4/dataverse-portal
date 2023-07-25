d37fb5565d70bea90f33c5e56ff531d635e8bfd4
lauren olivier <laurenzolivier@gmail.com>
2023-07-25 16:37:20 +0200

    refactor(useDropDirection): update dropDirections

--- src/ui/hook/useDropDirection.ts
+++ src/ui/hook/useDropDirection.ts
@@ -1,16 +1,14 @@
 import { useState, useEffect, useCallback, type RefObject } from 'react'
 import { useWindowResize } from './useWindowResize'
 
-type DropDirection = 'dropup' | 'dropdown'
+type DropDirection = 'up' | 'down'
 
 export const useDropDirection = (ref: RefObject<HTMLElement>): DropDirection => {
-  const [direction, setDirection] = useState<DropDirection>('dropdown')
+  const [direction, setDirection] = useState<DropDirection>('down')
 
   const updateDropDirection = useCallback(() => {
     if (ref.current) {
-      setDirection(
-        ref.current.getBoundingClientRect().y > window.innerHeight / 2 ? 'dropup' : 'dropdown'
-      )
+      setDirection(ref.current.getBoundingClientRect().y > window.innerHeight / 2 ? 'up' : 'down')
     }
   }, [ref])
 
