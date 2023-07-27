3bf42fb6c323c69513ef0f4b9f33c153aedac31b
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-07-27 14:01:12 +0200

    feat: add utilities to determine separators for a given locale

--- src/util/i18n/i18n.ts
+++ src/util/i18n/i18n.ts
@@ -3,6 +3,29 @@ import * as S from 'fp-ts/string'
 import * as O from 'fp-ts/Option'
 
 /**
+ * Determines the character used as the thousand separator for a locale.
+ *
+ * @param {string} locale - The locale to use for determining the thousand separator, e.g., 'en-US' or 'fr-FR'.
+ * @returns {string} - The character used as the thousand separator in the specified locale.
+ * @example thousandSeparatorForLocale('de-DE') // '.'
+ * @example thousandSeparatorForLocale('en-US') // ','
+ */
+
+export const thousandSeparatorForLocale = (locale: string): string =>
+  new Intl.NumberFormat(locale, { useGrouping: true }).format(1111).replace(/1/g, '')
+
+/**
+ * Determines the character used as the decimal separator for a locale.
+ *
+ * @param {string} locale - The locale to use for determining the decimal separator, e.g., 'en-US' or 'fr-FR'.
+ * @returns {string} - The character used as the decimal separator in the specified locale.
+ * @example decimalSeparatorForLocale('fr-FR') // ','
+ * @example decimalSeparatorForLocale('en-US') // '.'
+ */
+export const decimalSeparatorForLocale = (locale: string): string =>
+  new Intl.NumberFormat(locale, { useGrouping: false }).format(1.1).replace(/1/g, '')
+
+/**
  * Returns a function that formats a number according to the specified locale and options.
  * The returned function replaces the decimal separator with the specified one.
  *
@@ -14,16 +37,12 @@ import * as O from 'fp-ts/Option'
 export const localizedNumberFormatter =
   (locale: string, options: Intl.NumberFormatOptions, decimalSeparator?: string) =>
   (n: number): string => {
-    const formattedDecimalSeparator = new Intl.NumberFormat(locale, { useGrouping: false })
-      .format(1.1)
-      .replace(/1/g, '')
-
     const formattedNumber = n.toLocaleString(locale, options)
 
     return pipe(
       O.fromNullable(decimalSeparator),
       O.map(decimalSeparator =>
-        pipe(formattedNumber, S.replace(formattedDecimalSeparator, decimalSeparator))
+        pipe(formattedNumber, S.replace(decimalSeparatorForLocale(locale), decimalSeparator))
       ),
       O.getOrElse(() => formattedNumber)
     )
