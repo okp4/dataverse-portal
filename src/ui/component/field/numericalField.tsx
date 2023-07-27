88f45e8f3d06f7881eef7451e747e12914f10c93
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-07-27 14:05:32 +0200

    feat: customise locale and decimal limit & separator through props

--- src/ui/component/field/numericalField.tsx
+++ src/ui/component/field/numericalField.tsx
@@ -4,57 +4,90 @@ import * as S from 'fp-ts/string'
 import { pipe, flow } from 'fp-ts/function'
 import type { FieldProps } from '@/ui/component/field/field'
 import { Field } from '@/ui/component/field/field'
-import { localizedNumberFormatter } from '@/util/i18n/i18n'
-import { endsZeroDotted, numberFormatValidator } from '@/util/util'
+import {
+  decimalSeparatorForLocale,
+  localizedNumberFormatter,
+  thousandSeparatorForLocale
+} from '@/util/i18n/i18n'
+import { endsZeroDotted, escapeRegExp, numberFormatValidator } from '@/util/util'
 
-type NumericalFieldProps = FieldProps & Required<Pick<FieldProps, 'onChange'>>
-
-const coinDecimal = APP_ENV.chains[0].feeCurrencies[0].coinDecimals
+type NumericalFieldProps = FieldProps &
+  Required<Pick<FieldProps, 'onChange'>> & {
+    locale?: string
+    decimalSeparator?: '.' | ','
+    decimalLimit?: number
+  }
 
 // eslint-disable-next-line max-lines-per-function
 export const NumericalField: React.FC<NumericalFieldProps> = props => {
-  const { onChange, value } = props
+  const { onChange, value, locale = 'fr-FR', decimalSeparator, decimalLimit = 2 } = props
 
-  const thousandSeparator = '\u202F' // narrow no-break space
-  const decimalSeparator = '.'
+  const thousandSeparator = thousandSeparatorForLocale(locale)
+
+  if (thousandSeparator === decimalSeparator) {
+    throw new Error(
+      `Both 'thousandSeparator' and 'decimalSeparator' are set to '${thousandSeparator}' and they must be different for locale '${locale}'.\nEither set decimalSeparator to '${
+        decimalSeparator === '.' ? ',' : '.'
+      }' or change the locale.`
+    )
+  }
 
   const formatLocalizedNumber = useMemo(
     () =>
       localizedNumberFormatter(
-        'fr-FR',
+        locale,
         {
           useGrouping: true,
           minimumFractionDigits: 0,
-          maximumFractionDigits: coinDecimal
+          maximumFractionDigits: decimalLimit
         },
         decimalSeparator
       ),
-    []
+    [locale, decimalSeparator, decimalLimit]
   )
 
   const validateNumberFormat = useMemo(
-    () => numberFormatValidator(thousandSeparator, decimalSeparator, coinDecimal),
-    []
+    () =>
+      numberFormatValidator(
+        thousandSeparator,
+        decimalSeparator ?? decimalSeparatorForLocale(locale),
+        decimalLimit
+      ),
+    [thousandSeparator, decimalLimit, decimalSeparator, locale]
   )
 
   const handleInputChange = useCallback(
     (value: string) => {
       pipe(
         value,
-        S.replace(',', decimalSeparator),
-        S.replace(new RegExp(thousandSeparator, 'g'), ''),
+        S.replace(new RegExp(escapeRegExp(thousandSeparator), 'g'), ''),
+        S.replace(decimalSeparatorForLocale(locale), decimalSeparator ?? '$&'),
         validateNumberFormat,
         O.map(validValue =>
           pipe(
             validValue,
-            O.fromPredicate(s => S.isEmpty(s) || s.endsWith('.') || endsZeroDotted(s)),
+            O.fromPredicate(
+              s =>
+                S.isEmpty(s) ||
+                s.endsWith(decimalSeparator ?? decimalSeparatorForLocale(locale)) ||
+                endsZeroDotted(s)
+            ),
             O.map(flow(onChange)),
-            O.getOrElse(() => pipe(validValue, parseFloat, formatLocalizedNumber, onChange))
+            O.getOrElse(() =>
+              pipe(validValue, S.replace(',', '.'), parseFloat, formatLocalizedNumber, onChange)
+            )
           )
         )
       )
     },
-    [onChange, formatLocalizedNumber, validateNumberFormat]
+    [
+      onChange,
+      locale,
+      formatLocalizedNumber,
+      validateNumberFormat,
+      decimalSeparator,
+      thousandSeparator
+    ]
   )
 
   return <Field {...props} onChange={handleInputChange} value={value} />
