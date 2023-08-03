f3f4d8a36e972597e87a08fa55567ccf5ad200bb
Anja <a.miscevic@okp4.com>
2023-08-03 16:52:48 +0200

    feat: add modal animations

--- src/ui/component/modal/modal.tsx
+++ src/ui/component/modal/modal.tsx
@@ -1,6 +1,7 @@
-import { type ReactNode, type FC, useCallback, useRef, useState, useEffect } from 'react'
+import { type ReactNode, type FC, useCallback } from 'react'
 import { createPortal } from 'react-dom'
 import classNames from 'classnames'
+import { CSSTransition } from 'react-transition-group'
 import { useOnKeyboard } from '@/ui/hook/useOnKeyboard'
 import './modal.scss'
 
@@ -8,8 +9,6 @@ type ModalProps = {
   isOpen: boolean
   onClose: () => void
   closeOnEsc?: boolean
-  isCentered?: boolean
-  motionPreset?: 'zoom' | 'none'
   children?: ReactNode
   classes?: {
     main?: string
@@ -22,8 +21,6 @@ export const Modal: FC<ModalProps> = ({
   isOpen,
   onClose,
   closeOnEsc = true,
-  isCentered = true,
-  motionPreset = 'zoom',
   children,
   classes
 }) => {
@@ -44,40 +41,26 @@ export const Modal: FC<ModalProps> = ({
 
   const containerElement = document.getElementById('modal')
 
-  const prevIsOpenRef = useRef(isOpen)
-
-  const [isClosing, setIsClosing] = useState(false)
-
-  useEffect(() => {
-    if (prevIsOpenRef.current && !isOpen) {
-      setIsClosing(true)
-      const timeoutId = setTimeout(() => {
-        setIsClosing(false)
-      }, 200)
-      return () => clearTimeout(timeoutId)
-    }
-    prevIsOpenRef.current = isOpen
-  }, [isOpen])
-
   return (
-    isOpen &&
     containerElement &&
     createPortal(
-      <div
-        className={classNames('okp4-dataverse-portal-modal-main', classes?.overlay)}
+      <CSSTransition
+        // classNames={motionPreset === 'zoom' ? 'zoom-transition' : ''}
+        className="zoom-transition"
+        in={isOpen}
+        timeout={200}
+        unmountOnExit
+      >
         onClick={handleOverlayClick}
       >
         <div
-          className={classNames('okp4-dataverse-portal-modal-dialog', classes?.main, isCentered, {
-            zoom: motionPreset === 'zoom',
-            open: isOpen,
-            closing: isClosing
-          })}
+          className={classNames('okp4-dataverse-portal-modal-dialog', classes?.main)}
           role="dialog"
         >
           <div className="okp4-dataverse-portal-modal-content">{children}</div>
         </div>
-      </div>,
+        </div>
+      </CSSTransition>,
       containerElement
     )
   )
