f41556c28bbb484820e94751f212c8d02d05721b
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-08-04 17:20:49 +0200

    feat(domain): add vocabulary commands

---
+++ src/domain/vocabulary/command.ts
@@ -0,0 +1,23 @@
+import type { ReaderTaskEither } from 'fp-ts/ReaderTaskEither'
+import type { VocabularyPort } from './port'
+import type { HTTPNetworkError, NetworkUnspecifiedError } from '@/shared/error/network'
+import type { SerializationError } from '@/shared/error/serialize'
+
+export type Deps = {
+  vocabularyGateway: VocabularyPort
+  language: string
+  limit: number
+}
+
+export type VocabularyType = 'license' | 'topic' | 'media-type' | 'area'
+
+export type RetrieveVocabularyError =
+  | HTTPNetworkError
+  | NetworkUnspecifiedError
+  | SerializationError
+
+export type Command = {
+  retrieveVocabularyByType: (
+    type: VocabularyType
+  ) => ReaderTaskEither<Deps, RetrieveVocabularyError, void>
+}
