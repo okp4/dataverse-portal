8c0466c2f22d904354cdc9c8fe1db0ea67da3521
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-08-04 17:15:26 +0200

    feat(shared): move errors into error folder

---
+++ src/shared/error/serialize.ts
@@ -0,0 +1,24 @@
+import type { Show } from 'fp-ts/lib/Show'
+
+export const SerializationError = (reason: unknown) =>
+  ({
+    _tag: 'serialization',
+    reason
+  } as const)
+
+/**
+ *  Error when a serialization operation fails
+ */
+export type SerializationError = ReturnType<typeof SerializationError>
+
+export const ShowSerializationError: Show<SerializationError> = {
+  show: (error: SerializationError): string => {
+    switch (error._tag) {
+      case 'serialization': {
+        return `Error ${
+          error._tag
+        }: Failed to serialize data due to the following reason <${JSON.stringify(error.reason)}>`
+      }
+    }
+  }
+}
