750b9e8267079ece6f1939f6ffe8c6759dc1b82d
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-08-05 21:19:12 +0200

    test(domain): adjust helper test suite with mods

--- src/domain/vocabulary/test/helper.spec.ts
+++ src/domain/vocabulary/test/helper.spec.ts
@@ -1,7 +1,7 @@
-import type { Deps } from '../command'
 import * as E from 'fp-ts/Either'
+import type { RetrieveVocabularyDependencies } from '../helper/dependencies'
 import {
-  GatewayTypeError,
+  MandatoryDependenciesError,
   LanguageEmptyError,
   NegativeLimitError,
   createVocabularyDependenciesWithOptions,
@@ -11,27 +11,35 @@ import {
 } from '../helper/dependencies'
 import { pipe } from 'fp-ts/lib/function'
 import type { VocabularyPort } from '../port'
+// eslint-disable-next-line import/no-restricted-paths
+import { sparqlGateway } from '@/infra/vocabulary/sparql/sparqlGateway'
 
 type Data = {
   vocabularyGateway?: VocabularyPort
   language?: string
   limit?: number
-  expectedDeps: Deps
+  expectedDeps: RetrieveVocabularyDependencies
   expectedError?: Error
 }
 
 describe('Considering the createDepsWithOptions() function', () => {
+  // Expected Deps
+  const emptyDeps = Object.create(null)
+  const expectedDeps1: RetrieveVocabularyDependencies = {
+    _tag: 'retrieve-vocabulary-deps',
+    language: 'en',
+    limit: 20,
+    vocabularyGateway: sparqlGateway
+  }
   describe.each`
-    vocabularyGateway                | language     | limit        | expectedDeps                                                                       | expectedError
-    ${undefined}                     | ${undefined} | ${undefined} | ${Object.create(null)}                                                             | ${undefined}
-    ${{ foo: 'bar' }}                | ${''}        | ${undefined} | ${Object.create(null)}                                                             | ${GatewayTypeError({ foo: 'bar' })}
-    ${undefined}                     | ${''}        | ${undefined} | ${Object.create(null)}                                                             | ${LanguageEmptyError()}
-    ${undefined}                     | ${''}        | ${-1}        | ${Object.create(null)}                                                             | ${LanguageEmptyError()}
-    ${undefined}                     | ${undefined} | ${-1}        | ${Object.create(null)}                                                             | ${NegativeLimitError(-1)}
-    ${{ retrieveVocabulary: 'foo' }} | ${'en'}      | ${20}        | ${{ language: 'en', limit: 20, vocabularyGateway: { retrieveVocabulary: 'foo' } }} | ${undefined}
-    ${{ retrieveVocabulary: 'foo' }} | ${undefined} | ${undefined} | ${{ vocabularyGateway: { retrieveVocabulary: 'foo' } }}                            | ${undefined}
-    ${undefined}                     | ${'en'}      | ${undefined} | ${{ language: 'en' }}                                                              | ${undefined}
-    ${undefined}                     | ${undefined} | ${20}        | ${{ limit: 20 }}                                                                   | ${undefined}
+    vocabularyGateway | language     | limit        | expectedDeps     | expectedError
+    ${undefined}      | ${undefined} | ${undefined} | ${emptyDeps}     | ${MandatoryDependenciesError({})}
+    ${sparqlGateway}  | ${''}        | ${20}        | ${emptyDeps}     | ${LanguageEmptyError()}
+    ${sparqlGateway}  | ${'en'}      | ${-1}        | ${emptyDeps}     | ${NegativeLimitError(-1)}
+    ${sparqlGateway}  | ${'en'}      | ${20}        | ${expectedDeps1} | ${undefined}
+    ${sparqlGateway}  | ${undefined} | ${undefined} | ${emptyDeps}     | ${MandatoryDependenciesError({ vocabularyGateway: sparqlGateway })}
+    ${undefined}      | ${'en'}      | ${undefined} | ${emptyDeps}     | ${MandatoryDependenciesError({ language: 'en' })}
+    ${undefined}      | ${undefined} | ${20}        | ${emptyDeps}     | ${MandatoryDependenciesError({ limit: 20 })}
   `(
     'Given a vocabularyGateway <$vocabularyGateway>, a language <$language> and a limit <$limit>',
     ({ vocabularyGateway, language, limit, expectedDeps, expectedError }: Data) => {
