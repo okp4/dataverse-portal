c167bf6728a94cdd19f65655805346ee63cabc12
Anja <a.miscevic@okp4.com>
2023-08-10 16:57:14 +0200

    feat: add okp4 modal

--- src/ui/component/modal/modal.tsx
+++ src/ui/component/modal/modal.tsx
@@ -5,7 +5,7 @@ import { CSSTransition } from 'react-transition-group'
 import { useOnKeyboard } from '@/ui/hook/useOnKeyboard'
 import './modal.scss'
 
-type ModalProps = {
+export type ModalProps = {
   isOpen: boolean
   onClose: () => void
   closeOnEsc?: boolean
