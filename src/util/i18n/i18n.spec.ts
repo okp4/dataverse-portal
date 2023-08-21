c552e606ed65497fdc791d3905ee558edfe183f4
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-08-21 19:06:31 +0200

    test: formatter and parser for localized numbers

--- src/util/i18n/i18n.spec.ts
+++ src/util/i18n/i18n.spec.ts
@@ -1,4 +1,10 @@
-import { decimalSeparatorForLocale, thousandSeparatorForLocale } from './i18n'
+/* eslint-disable max-lines-per-function */
+import {
+  decimalSeparatorForLocale,
+  localizedNumberFormatter,
+  localizedNumberParser,
+  thousandSeparatorForLocale
+} from './i18n'
 
 describe('I18n utilities', () => {
   describe('thousandSeparatorForLocale', () => {
@@ -14,6 +20,94 @@ describe('I18n utilities', () => {
       expect(decimalSeparatorForLocale('fr-FR')).toEqual(',')
       expect(decimalSeparatorForLocale('en-US')).toEqual('.')
       expect(decimalSeparatorForLocale('de-DE')).toEqual(',')
+    })
+  })
+
+  describe('localizedNumberFormatter', () => {
+    describe.each([
+      [
+        'fr-FR',
+        { minimumFractionDigits: 0, maximumFractionDigits: 2 },
+        '.',
+        ' ',
+        1234.56,
+        '1 234.56'
+      ],
+      [
+        'fr-FR',
+        { minimumFractionDigits: 0, maximumFractionDigits: 2 },
+        '.',
+        undefined,
+        1234.56,
+        '1 234.56'
+      ],
+      [
+        'fr-FR',
+        { minimumFractionDigits: 0, maximumFractionDigits: 2 },
+        undefined,
+        undefined,
+        1234.568,
+        '1 234,57'
+      ],
+      [
+        'en-US',
+        { minimumFractionDigits: 0, maximumFractionDigits: 2 },
+        undefined,
+        undefined,
+        1234.568,
+        '1,234.57'
+      ],
+      [
+        'en-US',
+        { minimumFractionDigits: 0, maximumFractionDigits: 2 },
+        ',',
+        '.',
+        1234.568,
+        '1.234,57'
+      ]
+    ])(
+      'formatLocalizedNumber',
+      (locale, options, decimalSeparator, thousandSeparator, input, expectedOutput) => {
+        it(`formats a number according to the specified locale, options, and separators`, () => {
+          const formatter = localizedNumberFormatter(
+            options,
+            locale,
+            decimalSeparator,
+            thousandSeparator
+          )
+          expect(formatter(input)).toEqual(expectedOutput)
+        })
+      }
+    )
+
+    it('throws an error when decimal and thousand separators are the same', () => {
+      expect(() => {
+        localizedNumberFormatter({}, 'en-US', '.', '.')
+      }).toThrow('Decimal and thousand separators must be different')
+    })
+  })
+
+  describe('localizedNumberParser', () => {
+    describe.each([
+      ['fr-FR', '.', ' ', '1 234.56', 1234.56],
+      ['fr-FR', '.', undefined, '1 234.56', 1234.56],
+      ['fr-FR', undefined, undefined, '1 234,57', 1234.57],
+      ['en-US', undefined, undefined, '1,234.57', 1234.57],
+      ['en-US', ',', '.', '1.234,57', 1234.57]
+    ])(
+      'parseLocalizedNumber',
+      (locale, decimalSeparator, thousandSeparator, input, expectedOutput) => {
+        it(`parses a formatted string according to the specified locale and separators`, () => {
+          const parser = localizedNumberParser(locale, decimalSeparator, thousandSeparator)
+          expect(parser(input)).toEqual(expectedOutput)
+        })
+      }
+    )
+
+    it('throws an error when decimal and thousand separators are the same', () => {
+      expect(() => {
+        localizedNumberParser('en-US', '.', '.')
+      }).toThrow('Decimal and thousand separators must be different')
     })
   })
 })
