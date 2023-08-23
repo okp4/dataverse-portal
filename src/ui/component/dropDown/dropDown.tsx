578dd7a5c3cbe338372847cb1cc32d012be8abf7
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-08-23 09:04:27 +0200

    feat: make dropdown component more flexible with classnames props

--- src/ui/component/dropDown/dropDown.tsx
+++ src/ui/component/dropDown/dropDown.tsx
@@ -1,21 +1,33 @@
 import type { FC } from 'react'
+import classNames from 'classnames'
 import { Popover } from '@/ui/component/popover/popover'
 import './dropDown.scss'
 
 export type DropDownProps = {
   trigger: JSX.Element
   content: JSX.Element
+  triggerClassName?: string
+  contentClassName?: string
 }
 
-export const DropDown: FC<DropDownProps> = ({ trigger, content }) => {
+export const DropDown: FC<DropDownProps> = ({
+  trigger,
+  content,
+  triggerClassName,
+  contentClassName
+}) => {
   return (
     <div className="okp4-dataverse-portal-dropdown-main">
       <Popover
         align="start"
         content={content}
-        contentClassName="okp4-dataverse-portal-dropdown-content"
+        contentClassName={classNames('okp4-dataverse-portal-dropdown-content', contentClassName)}
         sideOffset={8}
-        trigger={<div className="okp4-dataverse-portal-dropdown-field">{trigger}</div>}
+        trigger={
+          <div className={classNames('okp4-dataverse-portal-dropdown-field', triggerClassName)}>
+            {trigger}
+          </div>
+        }
         triggerClassName="okp4-dataverse-portal-dropdown-trigger"
         triggerIconName="chevron-sharp"
       />
