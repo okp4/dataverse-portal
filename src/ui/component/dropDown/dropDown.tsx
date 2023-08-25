ca93d68461d0dd8a59303305457b737559ba8c87
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-08-25 09:53:07 +0200

    refactor: update component props with more appropriate names

--- src/ui/component/dropDown/dropDown.tsx
+++ src/ui/component/dropDown/dropDown.tsx
@@ -3,19 +3,19 @@ import { Popover } from '@/ui/component/popover/popover'
 import './dropDown.scss'
 
 export type DropDownProps = {
-  field: JSX.Element
-  options: JSX.Element
+  trigger: JSX.Element
+  content: JSX.Element
 }
 
-export const DropDown: FC<DropDownProps> = ({ field, options }) => {
+export const DropDown: FC<DropDownProps> = ({ trigger, content }) => {
   return (
     <div className="okp4-dataverse-portal-dropdown-main">
       <Popover
         align="start"
-        content={options}
+        content={content}
         contentClassName="okp4-dataverse-portal-dropdown-content"
         sideOffset={8}
-        trigger={<div className="okp4-dataverse-portal-dropdown-field">{field}</div>}
+        trigger={<div className="okp4-dataverse-portal-dropdown-field">{trigger}</div>}
         triggerClassName="okp4-dataverse-portal-dropdown-trigger"
         triggerIconName="chevron-sharp"
       />
