6ad267ad5c1ad0194b212aa15addd3e73b431ed1
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-09-08 13:04:36 +0200

    feat: create functions dedicated to date formatter and pattern

--- src/ui/component/dateInput/dateInput.tsx
+++ src/ui/component/dateInput/dateInput.tsx
@@ -2,6 +2,20 @@ import { useMemo, type FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Icon } from '@/ui/component/icon/icon'
 import './dateInput.scss'
+import type { PartialDateTimeFormatOptions } from '@/util/date/date'
+import { formatDateToPattern } from '@/util/date/date'
+
+const datePattern = {
+  day: 'DD',
+  month: 'MM',
+  year: 'YY'
+}
+
+const dateFormatOptions: PartialDateTimeFormatOptions = {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric'
+}
 
 type DateInputProps = {
   date?: Date
@@ -11,38 +25,20 @@ export const DateInput: FC<DateInputProps> = ({ date }) => {
   const { i18n } = useTranslation()
   const locale = i18n.language
 
-  const localizedDateFormatter = useMemo(
-    () => new Intl.DateTimeFormat(locale, { year: 'numeric', month: '2-digit', day: '2-digit' }),
-    [locale]
-  )
+  const dateFormatter = useMemo(() => new Intl.DateTimeFormat(locale, dateFormatOptions), [locale])
 
-  const localizedDatePattern = useMemo(
-    () =>
-      localizedDateFormatter
-        .formatToParts(date)
-        .map(({ type, value }) => {
-          switch (type) {
-            case 'day':
-              return 'DD'
-            case 'month':
-              return 'MM'
-            case 'year':
-              return 'YY'
-            default:
-              return value
-          }
-        })
-        .join(''),
-    [localizedDateFormatter, date]
+  const datePlaceholder = useMemo(
+    () => formatDateToPattern(datePattern, dateFormatOptions, locale),
+    [locale]
   )
 
   return (
     <div className="okp4-dataverse-portal-date-input-main">
       <input
         className="okp4-dataverse-portal-date-input"
-        placeholder={localizedDatePattern}
+        placeholder={datePlaceholder}
         readOnly
-        value={date ? localizedDateFormatter.format(date) : ''}
+        value={date ? dateFormatter.format(date) : ''}
       />
       <Icon name="calendar" />
     </div>
